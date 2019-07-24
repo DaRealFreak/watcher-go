cf285bf018dc71cb508d1b99c57ea8ddebe66bf4
Steffen Keuper <steffen.keuper@web.de>
2019-07-24 15:09:34 +0200

    [TASK] add AddAccountByUri and AddItemByUri functions, add watcher struct

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"watcher-go/database"
 )
+
+type watcher struct {
+	dbCon *database.DbIO
+}
 
 // main functionality of the database
 // logs fatal error if no DatabaseConnection could be established
 func main() {
-	dbCon := database.NewConnection()
-	module := "test"
-	fmt.Println("all items regardless of module: ", dbCon.GetTrackedItems(nil))
-	fmt.Println("all items of module: ", dbCon.GetTrackedItems(&module))
-	exampleItem := dbCon.GetFirstOrCreateTrackedItem("test_item", "test_module")
-	fmt.Println("example item persisted: ", exampleItem)
-	dbCon.ChangeTrackedItemCompleteStatus(&exampleItem, true)
-	fmt.Println("update example item completed", exampleItem)
-	dbCon.ChangeTrackedItemCompleteStatus(&exampleItem, false)
-	fmt.Println("update example item uncompleted", exampleItem)
+	watcher := watcher{}
+	watcher.dbCon = database.NewConnection()
+	// ToDO: iterate through all active items and run them
+	watcher.dbCon.CloseConnection()
+}
 
-	dbCon.CloseConnection()
+// extract the module based on the uri and add account if not registered already
+func (w watcher) AddAccountByUri(uri string, user string, password string) {
+	// ToDo: implement functionality to retrieve the module based on the uri
+	module := uri
+	w.dbCon.GetFirstOrCreateAccount(user, password, module)
+}
+
+// add item based on the uri and set it to the passed current item if not nil
+func (w watcher) AddItemByUri(uri string, currentItem string) {
+	// ToDo: implement functionality to retrieve the module based on the uri
+	module := uri
+	trackedItem := w.dbCon.GetFirstOrCreateTrackedItem(uri, module)
+	if currentItem != "" {
+		w.dbCon.UpdateTrackedItem(&trackedItem, currentItem)
+	}
 }
