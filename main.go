7abdea857dbca7fee3ed83a87c1294e1a371f682
DaRealFreak <steffen.keuper@web.de>
2019-07-25 01:07:11 +0200

    [TASK] rename main application from w to app

--- main.go
+++ main.go
@@ -18,18 +18,18 @@ func main() {
 }
 
 // extract the module based on the uri and add account if not registered already
-func (w watcher) AddAccountByUri(uri string, user string, password string) {
+func (app watcher) AddAccountByUri(uri string, user string, password string) {
 	// ToDo: implement functionality to retrieve the module based on the uri
 	module := uri
-	w.dbCon.GetFirstOrCreateAccount(user, password, module)
+	app.dbCon.GetFirstOrCreateAccount(user, password, module)
 }
 
 // add item based on the uri and set it to the passed current item if not nil
-func (w watcher) AddItemByUri(uri string, currentItem string) {
+func (app watcher) AddItemByUri(uri string, currentItem string) {
 	// ToDo: implement functionality to retrieve the module based on the uri
 	module := uri
-	trackedItem := w.dbCon.GetFirstOrCreateTrackedItem(uri, module)
+	trackedItem := app.dbCon.GetFirstOrCreateTrackedItem(uri, module)
 	if currentItem != "" {
-		w.dbCon.UpdateTrackedItem(&trackedItem, currentItem)
+		app.dbCon.UpdateTrackedItem(&trackedItem, currentItem)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 	"watcher-go/database"
 )
 
-var w watcher
+var app watcher
 
 // main function for tests
 // remove previous database to prevent previous data influencing the tests
@@ -15,23 +15,23 @@ var w watcher
 func TestMain(m *testing.M) {
 	// constructor
 	database.RemoveDatabase()
-	w = watcher{}
-	w.dbCon = database.NewConnection()
+	app = watcher{}
+	app.dbCon = database.NewConnection()
 
 	// run the unit tests
 	code := m.Run()
 
 	// destructor
-	w.dbCon.CloseConnection()
+	app.dbCon.CloseConnection()
 	database.RemoveDatabase()
 	os.Exit(code)
 }
 
 // test the add account function
 func TestAddAccountByUri(t *testing.T) {
-	w.AddAccountByUri("https://www.test.com", "user", "pass")
+	app.AddAccountByUri("https://www.test.com", "user", "pass")
 
-	account := w.dbCon.GetFirstOrCreateAccount("user", "different_pass", "https://www.test.com")
+	account := app.dbCon.GetFirstOrCreateAccount("user", "different_pass", "https://www.test.com")
 	if account.Password != "pass" {
 		t.Fatal("password got updated or different user got added")
 	}
@@ -39,18 +39,18 @@ func TestAddAccountByUri(t *testing.T) {
 
 // test the add item by uri function
 func TestAddItemByUri(t *testing.T) {
-	w.AddItemByUri("test_item", "")
-	w.AddItemByUri("test_item_with_current_item", "hi there")
+	app.AddItemByUri("test_item", "")
+	app.AddItemByUri("test_item_with_current_item", "hi there")
 
 	// ToDo: check generated items
 	module := "test_item"
-	fmt.Println("all items regardless of module: ", w.dbCon.GetTrackedItems(nil))
-	fmt.Println("all items of module: ", w.dbCon.GetTrackedItems(&module))
+	fmt.Println("all items regardless of module: ", app.dbCon.GetTrackedItems(nil))
+	fmt.Println("all items of module: ", app.dbCon.GetTrackedItems(&module))
 
-	exampleItem := w.dbCon.GetFirstOrCreateTrackedItem("test_item", module)
+	exampleItem := app.dbCon.GetFirstOrCreateTrackedItem("test_item", module)
 	fmt.Println("example item persisted: ", exampleItem)
-	w.dbCon.ChangeTrackedItemCompleteStatus(&exampleItem, true)
+	app.dbCon.ChangeTrackedItemCompleteStatus(&exampleItem, true)
 	fmt.Println("update example item completed", exampleItem)
-	w.dbCon.ChangeTrackedItemCompleteStatus(&exampleItem, false)
+	app.dbCon.ChangeTrackedItemCompleteStatus(&exampleItem, false)
 	fmt.Println("update example item uncompleted", exampleItem)
 }
