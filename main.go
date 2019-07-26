d1d4c74bcb552b0a365d646a7f52fbad4e3be8f1
Steffen Keuper <steffen.keuper@web.de>
2019-07-26 17:18:30 +0200

    [TASK] use module factory function to retrieve the module from the uri schemas, add basic main functionality

--- main.go
+++ main.go
@@ -18,21 +18,24 @@ func main() {
 		moduleFactory: modules.NewModuleFactory(),
 	}
 
+	watcher.AddAccountByUri("https://chan.sankakucomplex.com/", "user", "name")
+	module, _ := watcher.moduleFactory.GetModuleFromUri("https://chan.sankakucomplex.com/")
+	account := watcher.dbCon.GetAccount(module)
+	module.Module.Login(account)
+
 	// ToDO: iterate through all active items and run them
 	watcher.dbCon.CloseConnection()
 }
 
 // extract the module based on the uri and add account if not registered already
 func (app watcher) AddAccountByUri(uri string, user string, password string) {
-	// ToDo: implement functionality to retrieve the module based on the uri
-	module := uri
+	module, _ := app.moduleFactory.GetModuleFromUri(uri)
 	app.dbCon.GetFirstOrCreateAccount(user, password, module)
 }
 
 // add item based on the uri and set it to the passed current item if not nil
 func (app watcher) AddItemByUri(uri string, currentItem string) {
-	// ToDo: implement functionality to retrieve the module based on the uri
-	module := uri
+	module, _ := app.moduleFactory.GetModuleFromUri(uri)
 	trackedItem := app.dbCon.GetFirstOrCreateTrackedItem(uri, module)
 	if currentItem != "" {
 		app.dbCon.UpdateTrackedItem(&trackedItem, currentItem)
