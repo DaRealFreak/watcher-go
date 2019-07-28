517bd82d11e6e2bb044f1b137108d4fc41123bc9
DaRealFreak <steffen.keuper@web.de>
2019-07-28 02:19:00 +0200

    [TASK] add additional interface function IsLoggedIn(), add return value for login

--- cmd/watcher/models/module.go
+++ cmd/watcher/models/module.go
@@ -6,8 +6,9 @@ import (
 
 type ModuleInterface interface {
 	Key() (key string)
-	RegisterUriSchema(map[string][]*regexp.Regexp)
-	Login(account *Account)
+	IsLoggedIn() (loggedIn bool)
+	RegisterUriSchema(uriSchemas map[string][]*regexp.Regexp)
+	Login(account *Account) (success bool)
 	Parse(item *TrackedItem)
 }
 
