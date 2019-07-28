60e9714f091d15c2f56fedc410fdda64b905e13d
DaRealFreak <steffen.keuper@web.de>
2019-07-28 02:20:08 +0200

    [TASK] use pointer for interface implementation, add check if login succeeded, add new interface functions

--- cmd/watcher/modules/sankakucomplex/main.go
+++ cmd/watcher/modules/sankakucomplex/main.go
@@ -2,21 +2,28 @@ package sankakucomplex
 
 import (
 	"fmt"
+	"github.com/golang/glog"
+	"net/http"
 	"net/url"
 	"regexp"
-	"watcher-go/cmd/watcher/http"
+	"strings"
+	"time"
+	"watcher-go/cmd/watcher/http_wrapper"
 	"watcher-go/cmd/watcher/models"
 )
 
 type SankakuComplex struct {
-	session *http.Session
+	session  *http_wrapper.Session
+	loggedIn bool
 }
 
 // generate new module and register uri schema
 func NewModule(uriSchemas map[string][]*regexp.Regexp) *models.Module {
-	var templateImplementation models.ModuleInterface = SankakuComplex{
-		session: http.NewSession(),
+	var subModule = SankakuComplex{
+		session:  http_wrapper.NewSession(),
+		loggedIn: false,
 	}
+	var templateImplementation models.ModuleInterface = &subModule
 
 	module := models.Module{
 		Module: templateImplementation,
@@ -27,30 +34,49 @@ func NewModule(uriSchemas map[string][]*regexp.Regexp) *models.Module {
 }
 
 // retrieve the module key
-func (m SankakuComplex) Key() (key string) {
+func (m *SankakuComplex) Key() (key string) {
 	return "chan.sankakucomplex.com"
 }
 
+// retrieve the logged in status
+func (m *SankakuComplex) IsLoggedIn() (loggedIn bool) {
+	return m.loggedIn
+}
+
 // add our pattern to the uri schemas
-func (m SankakuComplex) RegisterUriSchema(uriSchemas map[string][]*regexp.Regexp) {
+func (m *SankakuComplex) RegisterUriSchema(uriSchemas map[string][]*regexp.Regexp) {
 	var moduleUriSchemas []*regexp.Regexp
 	test, _ := regexp.Compile(".*.sankakucomplex.com")
 	moduleUriSchemas = append(moduleUriSchemas, test)
 	uriSchemas[m.Key()] = moduleUriSchemas
 }
 
-func (m SankakuComplex) Login(account *models.Account) {
+// login function
+func (m *SankakuComplex) Login(account *models.Account) bool {
 	values := url.Values{
 		"url":            {""},
 		"user[name]":     {account.Username},
 		"user[password]": {account.Password},
 		"commit":         {"Login"},
 	}
-	// ToDo: validation of login response
-	_, _ = m.session.Post("https://chan.sankakucomplex.com/user/authenticate", values, 0)
+
+	res, _ := m.Post("https://chan.sankakucomplex.com/user/authenticate", values, 0)
+	htmlResponse, _ := m.session.GetDocument(res).Html()
+	m.loggedIn = strings.Contains(htmlResponse, "You are now logged in")
+	return m.loggedIn
 }
 
-func (m SankakuComplex) Parse(item *models.TrackedItem) {
-	fmt.Println(item.Uri, item.CurrentItem)
+func (m *SankakuComplex) Parse(item *models.TrackedItem) {
 	panic("implement me")
+}
+
+// custom Post function to check for specific status codes and messages
+func (m *SankakuComplex) Post(url string, data url.Values, tries int) (*http.Response, error) {
+	res, err := m.session.Post("https://chan.sankakucomplex.com/user/authenticate", data, tries)
+	if err == nil && res.StatusCode == 429 {
+		glog.Info(fmt.Sprintf("too many requests, sleeping '%d' seconds", tries+1*60))
+		time.Sleep(time.Duration(tries+1*60) * time.Second)
+		return m.Post(url, data, tries+1)
+	}
+	return res, err
 }
