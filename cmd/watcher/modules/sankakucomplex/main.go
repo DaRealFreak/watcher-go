d3b8e3d75fae428e5c3b2f7aada3725fc273b227
DaRealFreak <steffen.keuper@web.de>
2019-07-28 20:42:08 +0200

    [TASK] implement API handling and download queue generation

--- cmd/watcher/modules/sankakucomplex/main.go
+++ cmd/watcher/modules/sankakucomplex/main.go
@@ -1,8 +1,11 @@
 package sankakucomplex
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/golang/glog"
+	"github.com/kubernetes/klog"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -17,6 +20,69 @@ type SankakuComplex struct {
 	dbCon    *database.DbIO
 	session  *http_wrapper.Session
 	loggedIn bool
+}
+
+type ApiItem struct {
+	Id               int
+	Rating           string
+	Status           string
+	Author           Author
+	SampleUrl        string `json:"sample_url"`
+	SampleWidth      int    `json:"sample_width"`
+	SampleHeight     int    `json:"sample_height"`
+	PreviewUrl       string `json:"preview_url"`
+	PreviewWidth     int    `json:"preview_width"`
+	FileUrl          string `json:"file_url"`
+	Width            int
+	Height           int
+	FileSize         int         `json:"file_size"`
+	FileType         string      `json:"file_type"`
+	CreatedAt        Created     `json:"created_at"`
+	HasChildren      bool        `json:"has_children"`
+	HasComments      bool        `json:"has_comments"`
+	HasNotes         bool        `json:"has_notes"`
+	IsFavorite       bool        `json:"is_favorited"`
+	UserVote         json.Number `json:"user_vote"`
+	Md5              string
+	ParentId         json.Number `json:"parent_id"`
+	Change           int
+	FavCount         json.Number `json:"fav_count"`
+	RecommendedPosts json.Number `json:"recommended_posts"`
+	RecommendedScore json.Number `json:"recommended_score"`
+	VoteCount        json.Number `json:"vote_count"`
+	TotalScore       json.Number `json:"total_score"`
+	CommentCount     json.Number `json:"comment_count"`
+	Source           string
+	InVisiblePool    bool `json:"in_visible_pool"`
+	IsPremium        bool `json:"is_premium"`
+	Sequence         json.Number
+	Tags             []Tag
+}
+
+type Author struct {
+	Id           int
+	Name         string
+	Avatar       string
+	AvatarRating string `json:"avatar_rating"`
+}
+
+type Created struct {
+	JsonClass string `json:"json_class"`
+	S         int
+	N         int
+}
+
+type Tag struct {
+	Id        int
+	NameEn    string `json:"name_en"`
+	NameJa    string `json:"name_ja"`
+	Type      int
+	Count     int
+	PostCount int `json:"post_count"`
+	PoolCount int `json:"pool_count"`
+	Locale    string
+	Rating    json.Number
+	Name      string
 }
 
 // generate new module and register uri schema
@@ -67,16 +133,76 @@ func (m *SankakuComplex) Login(account *models.Account) bool {
 }
 
 func (m *SankakuComplex) Parse(item *models.TrackedItem) {
-	panic("implement me")
+	tag := m.ExtractItemTag(item)
+	page := 0
+	foundCurrentItem := false
+	var downloadQueue []models.DownloadQueueItem
+
+	for foundCurrentItem == false {
+		page += 1
+		apiUri := fmt.Sprintf("https://capi-v2.sankakucomplex.com/posts?lang=english&page=%d&limit=100&tags=%s", page, tag)
+		response, _ := m.Get(apiUri, 0)
+		apiItems := m.ParseApiResponse(response)
+		for _, data := range apiItems {
+			if data.Id != item.Id {
+				downloadQueue = append(downloadQueue, models.DownloadQueueItem{
+					ItemId:      string(item.Id),
+					DownloadTag: "test",
+					FileName:    "test.png",
+					FileUri:     data.FileUrl,
+				})
+			} else {
+				foundCurrentItem = true
+				break
+			}
+		}
+
+		// we reached the last possible page, break here
+		if len(apiItems) != 100 {
+			break
+		}
+
+	}
+
+	fmt.Println(downloadQueue)
 }
 
-// custom Post function to check for specific status codes and messages
-func (m *SankakuComplex) Post(url string, data url.Values, tries int) (*http.Response, error) {
-	res, err := m.session.Post("https://chan.sankakucomplex.com/user/authenticate", data, tries)
+// parse the response from the API
+func (m *SankakuComplex) ParseApiResponse(response *http.Response) []ApiItem {
+	body, _ := ioutil.ReadAll(response.Body)
+	var apiItems []ApiItem
+	_ = json.Unmarshal(body, &apiItems)
+	return apiItems
+}
+
+// extract the tag from the passed item to use in the API request
+func (m *SankakuComplex) ExtractItemTag(item *models.TrackedItem) string {
+	u, _ := url.Parse(item.Uri)
+	q, _ := url.ParseQuery(u.RawQuery)
+	if len(q["tags"]) == 0 {
+		log.Fatalf("parsed uri(%s) does not contain any \"tags\" tag", item.Uri)
+	}
+	return q["tags"][0]
+}
+
+// custom POST function to check for specific status codes and messages
+func (m *SankakuComplex) Post(uri string, data url.Values, tries int) (*http.Response, error) {
+	res, err := m.session.Post(uri, data, tries)
 	if err == nil && res.StatusCode == 429 {
-		glog.Info(fmt.Sprintf("too many requests, sleeping '%d' seconds", tries+1*60))
+		klog.Info(fmt.Sprintf("too many requests, sleeping '%d' seconds", tries+1*60))
 		time.Sleep(time.Duration(tries+1*60) * time.Second)
-		return m.Post(url, data, tries+1)
+		return m.Post(uri, data, tries+1)
+	}
+	return res, err
+}
+
+// custom GET function to check for specific status codes and messages
+func (m *SankakuComplex) Get(uri string, tries int) (*http.Response, error) {
+	res, err := m.session.Get(uri, tries)
+	if err == nil && res.StatusCode == 429 {
+		klog.Info(fmt.Sprintf("too many requests, sleeping '%d' seconds", tries+1*60))
+		time.Sleep(time.Duration(tries+1*60) * time.Second)
+		return m.Get(uri, tries+1)
 	}
 	return res, err
 }
