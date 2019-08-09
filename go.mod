d7f510ca7a1144e547bb914ef9fd8227ea34dc24
DaRealFreak <steffen.keuper@web.de>
2019-08-09 22:39:35 +0200

    [TASK] add update functionality to application

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.12
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/blang/semver v3.5.1+incompatible
 	github.com/mattn/go-sqlite3 v1.11.0
+	github.com/rhysd/go-github-selfupdate v1.1.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.3.2
---
+++ pkg/update/update.go
@@ -0,0 +1,63 @@
+package update
+
+import (
+	"fmt"
+	"github.com/DaRealFreak/watcher-go/pkg/version"
+	"github.com/blang/semver"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+type updateChecker struct {
+}
+
+// returns a new update checker
+func NewUpdateChecker() *updateChecker {
+	return &updateChecker{}
+}
+
+// check if any new releases exist and ask if we should update the binary
+func (u *updateChecker) CheckForAvailableUpdates() (err error) {
+	latest, found, err := selfupdate.DetectLatest(version.RepositoryUrl)
+	if err != nil {
+		log.Warning("error occurred while detecting version: ", err)
+		return err
+	}
+
+	v := semver.MustParse(version.VERSION)
+	if !found || latest.Version.LTE(v) {
+		log.Info("current version is the latest")
+		return nil
+	}
+
+	fmt.Printf("a new version (%s) is available, do you want to update? (y/n)\n", latest.Version.String())
+	// user doesn't want to update
+	if !u.askYesNoAnswer() {
+		return nil
+	}
+
+	exe, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("could not locate executable path")
+	}
+	if err := selfupdate.UpdateTo(latest.AssetURL, exe); err != nil {
+		return err
+	}
+	log.Info("Successfully updated to version " + latest.Version.String())
+	return nil
+}
+
+func (u *updateChecker) askYesNoAnswer() bool {
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if response != "y" && response != "n" {
+		fmt.Println("Invalid input, please write y or n")
+		return u.askYesNoAnswer()
+	} else {
+		return response == "y"
+	}
+}
