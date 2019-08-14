1edabc48ec5ec67ada5837aa790bc40aa7886e38
DaRealFreak <steffen.keuper@web.de>
2019-08-14 22:10:12 +0200

    [TASK] split up checking for updates into check and actual update function

--- pkg/update/update.go
+++ pkg/update/update.go
@@ -17,24 +17,35 @@ func NewUpdateChecker() *updateChecker {
 	return &updateChecker{}
 }
 
-// check if any new releases exist and ask if we should update the binary
-func (u *updateChecker) CheckForAvailableUpdates() (err error) {
+// check if any new releases exist
+func (u *updateChecker) CheckForAvailableUpdates() (updateAvailable bool, err error) {
 	latest, found, err := selfupdate.DetectLatest(version.RepositoryUrl)
 	if err != nil {
 		log.Warning("error occurred while detecting version: ", err)
-		return err
+		return false, err
 	}
 
 	v := semver.MustParse(version.VERSION)
 	if !found || latest.Version.LTE(v) {
-		log.Trace("current version is the latest")
+		return false, nil
+	}
+	return true, nil
+}
+
+// update the application
+func (u *updateChecker) UpdateApplication() (err error) {
+	updateAvailable, err := u.CheckForAvailableUpdates()
+	if !updateAvailable {
+		fmt.Println("current version is the latest")
 		return nil
 	}
 
-	fmt.Printf("a new version (%s) is available, do you want to update? (y/n)\n", latest.Version.String())
-	// user doesn't want to update
-	if !u.askYesNoAnswer() {
-		return nil
+	fmt.Println("new version detected, updating...")
+	// retrieve latest asset url again
+	latest, _, err := selfupdate.DetectLatest(version.RepositoryUrl)
+	if err != nil {
+		log.Warning("error occurred while retrieving latest asset URLs: ", err)
+		return err
 	}
 
 	exe, err := os.Executable()
@@ -44,20 +55,6 @@ func (u *updateChecker) CheckForAvailableUpdates() (err error) {
 	if err := selfupdate.UpdateTo(latest.AssetURL, exe); err != nil {
 		return err
 	}
-	log.Info("Successfully updated to version " + latest.Version.String())
+	log.Info("successfully updated to version " + latest.Version.String())
 	return nil
-}
-
-func (u *updateChecker) askYesNoAnswer() bool {
-	var response string
-	_, err := fmt.Scanln(&response)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if response != "y" && response != "n" {
-		fmt.Println("Invalid input, please write y or n")
-		return u.askYesNoAnswer()
-	} else {
-		return response == "y"
-	}
 }
