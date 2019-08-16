e4f50fdd53b4bb5eb8f6c5277edcc1e7cf67248b
DaRealFreak <steffen.keuper@web.de>
2019-08-17 01:15:11 +0200

    [TASK] don't expose functions, add general function to create animated image with ImageMagick, return error instead of logging fatal to allow fallbacks, use proper temporary directory

--- pkg/animation/animation.go
+++ pkg/animation/animation.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/google/uuid"
-	"github.com/mitchellh/go-homedir"
 	_ "golang.org/x/image/webp"
 	"image"
 	_ "image/gif"
@@ -12,7 +11,6 @@ import (
 	_ "image/png"
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -27,11 +25,30 @@ type FileData struct {
 	WorkPath  string
 }
 
-func CreateWebpAnimation(fileData *FileData) ([]byte, error) {
+type Helper struct {
+	outputDirectory string
+	outputFileName  string
+}
+
+// retrieve animation helper with standard settings
+func NewAnimationHelper() *Helper {
+	return &Helper{
+		outputFileName:  "output",
+		outputDirectory: os.TempDir(),
+	}
+}
+
+// internal function to create mkv video from the passed frames with the option
+// to not remove the temporary folder for further conversions from mkv to another format
+// (useful for webp/fliff animated image formats which are not directly supported by ImageMagick yet)
+func (h *Helper) createAnimationImageMagick(fileData *FileData, fileExtension string, deleteAfterConversion bool) (content []byte, err error) {
 	if len(fileData.Frames) != len(fileData.MsDelays) {
 		return nil, fmt.Errorf("delays don't match the frame count")
 	}
-	dumpFramesForImageMagick(fileData)
+
+	if err := h.dumpFramesForImageMagick(fileData); err != nil {
+		return nil, err
+	}
 
 	var executable string
 	var args []string
@@ -44,69 +61,63 @@ func CreateWebpAnimation(fileData *FileData) ([]byte, error) {
 	for i := 0; i <= len(fileData.Frames)-1; i++ {
 		args = append(args, "-delay", strconv.Itoa(fileData.MsDelays[i]/10), fileData.FilePaths[i])
 	}
-	args = append(args, "-loop", "0", filepath.Join(fileData.WorkPath, "output.mkv"))
+	args = append(args, "-loop", "0", filepath.Join(fileData.WorkPath, h.outputFileName+"."+fileExtension))
 
-	// ToDo: fallback to imaging library of golang of error
 	if err := exec.Command(executable, args...).Run(); err != nil {
-		log.Fatal(err)
-	}
-
-	// convert from mkv to webp due to ImageMagick not supporting animated webp yet
-	args = []string{"-y", "-i", filepath.Join(fileData.WorkPath, "output.mkv"), "-lossless", "1", "-loop", "0", filepath.Join(fileData.WorkPath, "output.webp")}
-	if err := exec.Command("ffmpeg", args...).Run(); err != nil {
-		log.Fatal(err)
+		fmt.Println("wat")
+		return nil, err
 	}
 
 	// read file content to return it
-	content, err := ioutil.ReadFile(filepath.Join(fileData.WorkPath, "output.webp"))
+	content, err = ioutil.ReadFile(filepath.Join(fileData.WorkPath, h.outputFileName+"."+fileExtension))
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	// clean up the created folder/files
-	if err := os.RemoveAll(fileData.WorkPath); err != nil {
-		log.Fatal(err)
+	// option to keep converted mkv for further conversions
+	if deleteAfterConversion {
+		// clean up the created folder/files
+		err = os.RemoveAll(fileData.WorkPath)
 	}
-	return content, nil
+	return content, err
 }
 
 // dump all frames into a unique folder for the ImageMagick conversion
-func dumpFramesForImageMagick(fData *FileData) {
+func (h *Helper) dumpFramesForImageMagick(fData *FileData) (err error) {
 	uuid4, err := uuid.NewUUID()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	// convert to the absolute path
-	homeDir, err := homedir.Dir()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fData.WorkPath = filepath.Join(homeDir, ".watcher", uuid4.String())
+	// create custom folder in the temporary directory
+	fData.WorkPath = filepath.Join(h.outputDirectory, uuid4.String())
 
 	// create the directory
 	if err := os.MkdirAll(fData.WorkPath, os.ModePerm); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
+	// reset file paths to allow multiple conversions of one FileData struct
+	fData.FilePaths = []string{}
 	// dump frames into folder and append created file paths into the file data
 	for index, frame := range fData.Frames {
 		// guess the image format for the file extension
-		fType, err := guessImageFormat(bytes.NewReader(frame))
+		fType, err := h.guessImageFormat(bytes.NewReader(frame))
 		fPath := filepath.Join(fData.WorkPath, fmt.Sprintf("%d.%s", index, fType))
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		err = ioutil.WriteFile(fPath, frame, 0644)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		fData.FilePaths = append(fData.FilePaths, fPath)
 	}
+	return nil
 }
 
 // guess image format from gif/jpeg/png/webp
-func guessImageFormat(r io.Reader) (format string, err error) {
+func (h *Helper) guessImageFormat(r io.Reader) (format string, err error) {
 	_, format, err = image.DecodeConfig(r)
 	return
 }
