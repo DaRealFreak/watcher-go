8c66217bc9e5a82dba8f6eb6fc254bc54cfbc56b
DaRealFreak <steffen.keuper@web.de>
2019-08-17 16:24:39 +0200

    [TASK] implement gif creation without 3rd party tools as fallback

--- pkg/animation/gif.go
+++ pkg/animation/gif.go
@@ -1,6 +1,18 @@
 package animation
 
-import log "github.com/sirupsen/logrus"
+import (
+	"bytes"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	_ "golang.org/x/image/bmp"
+	"golang.org/x/image/draw"
+	"image"
+	"image/color/palette"
+	"image/gif"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+)
 
 // create gif animated picture from the passed FileData
 func (h *Helper) CreateAnimationGif(fileData *FileData) (content []byte, err error) {
@@ -21,6 +33,27 @@ func (h *Helper) createAnimationGifImageMagick(fileData *FileData) (content []by
 // fallback function to create a gif file with the golang image libraries
 // quality suffers a lot (256 colors max f.e.), so ImageMagick conversion would be preferable
 func (h *Helper) createAnimationGifGo(fileData *FileData) (content []byte, err error) {
-	// ToDo: implement again
-	return
+	if len(fileData.Frames) != len(fileData.MsDelays) {
+		return nil, fmt.Errorf("delays don't match the frame count")
+	}
+
+	outGif := &gif.GIF{}
+	for i := 0; i <= len(fileData.Frames)-1; i++ {
+		decodedImage, _, err := image.Decode(bytes.NewReader(fileData.Frames[i]))
+		if err != nil {
+			return nil, err
+		}
+		// create new paletted image and draw our decoded image into it
+		imageFilePaletted := image.NewPaletted(decodedImage.Bounds(), palette.Plan9)
+		draw.Draw(imageFilePaletted, imageFilePaletted.Rect, decodedImage, decodedImage.Bounds().Min, draw.Over)
+
+		// append the new paletted image and the delay to the image
+		outGif.Image = append(outGif.Image, imageFilePaletted)
+		outGif.Delay = append(outGif.Delay, 0)
+	}
+	f := new(bytes.Buffer)
+	if err := gif.EncodeAll(f, outGif); err != nil {
+		return nil, err
+	}
+	return f.Bytes(), nil
 }
