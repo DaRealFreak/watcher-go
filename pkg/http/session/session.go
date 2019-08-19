d4c5c6cb9f7c9570c856367b6704cd36b3e6f9cf
DaRealFreak <steffen.keuper@web.de>
2019-08-20 01:18:11 +0200

    [TASK] add optional rate limiter to default session as build-up for issue #4

--- pkg/http/session/session.go
+++ pkg/http/session/session.go
@@ -1,9 +1,11 @@
 package session
 
 import (
+	"context"
 	"fmt"
 	watcherHttp "github.com/DaRealFreak/watcher-go/pkg/http"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/time/rate"
 	"io"
 	"net/http"
 	"net/http/cookiejar"
@@ -14,8 +16,10 @@ import (
 
 type DefaultSession struct {
 	watcherHttp.Session
-	Client     *http.Client
-	MaxRetries int
+	Client      *http.Client
+	RateLimiter *rate.Limiter
+	MaxRetries  int
+	ctx         context.Context
 }
 
 // initialize a new session and set all the required headers etc
@@ -25,17 +29,20 @@ func NewSession() *DefaultSession {
 	app := DefaultSession{
 		Client:     &http.Client{Jar: jar},
 		MaxRetries: 5,
+		ctx:        context.Background(),
 	}
 	return &app
 }
 
 // sends a GET request, returns the occurred error if something went wrong even after multiple tries
-func (session *DefaultSession) Get(uri string) (response *http.Response, err error) {
+func (s *DefaultSession) Get(uri string) (response *http.Response, err error) {
 	// access the passed url and return the data or the error which persisted multiple retries
 	// post the request with the retries option
-	for try := 1; try <= session.MaxRetries; try++ {
+	for try := 1; try <= s.MaxRetries; try++ {
+		s.applyRateLimit()
+
 		log.Debug(fmt.Sprintf("opening GET uri \"%s\" (try: %d)", uri, try))
-		response, err = session.Client.Get(uri)
+		response, err = s.Client.Get(uri)
 		// if no error occurred break out of the loop
 		if err == nil {
 			break
@@ -47,11 +54,13 @@ func (session *DefaultSession) Get(uri string) (response *http.Response, err err
 }
 
 // sends a POST request, returns the occurred error if something went wrong even after multiple tries
-func (session *DefaultSession) Post(uri string, data url.Values) (response *http.Response, err error) {
+func (s *DefaultSession) Post(uri string, data url.Values) (response *http.Response, err error) {
 	// post the request with the retries option
-	for try := 1; try <= session.MaxRetries; try++ {
+	for try := 1; try <= s.MaxRetries; try++ {
+		s.applyRateLimit()
+
 		log.Debug(fmt.Sprintf("opening POST uri \"%s\" (try: %d)", uri, try))
-		response, err = session.Client.PostForm(uri, data)
+		response, err = s.Client.PostForm(uri, data)
 		// if no error occurred break out of the loop
 		if err == nil {
 			break
@@ -63,10 +72,10 @@ func (session *DefaultSession) Post(uri string, data url.Values) (response *http
 }
 
 // try to download the file, returns the occurred error if something went wrong even after multiple tries
-func (session *DefaultSession) DownloadFile(filepath string, uri string) (err error) {
-	for try := 1; try <= session.MaxRetries; try++ {
+func (s *DefaultSession) DownloadFile(filepath string, uri string) (err error) {
+	for try := 1; try <= s.MaxRetries; try++ {
 		log.Info(fmt.Sprintf("downloading file: \"%s\" (uri: %s, try: %d)", filepath, uri, try))
-		err = session.tryDownloadFile(filepath, uri)
+		err = s.tryDownloadFile(filepath, uri)
 		// if no error occurred return nil
 		if err == nil {
 			return
@@ -79,16 +88,16 @@ func (session *DefaultSession) DownloadFile(filepath string, uri string) (err er
 
 // this function will download a url to a local file.
 // It's efficient because it will write as it downloads and not load the whole file into memory.
-func (session *DefaultSession) tryDownloadFile(filepath string, uri string) error {
+func (s *DefaultSession) tryDownloadFile(filepath string, uri string) error {
 	// retrieve the data
-	resp, err := session.Get(uri)
+	resp, err := s.Get(uri)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
 	// ensure the directory
-	session.EnsureDownloadDirectory(filepath)
+	s.EnsureDownloadDirectory(filepath)
 
 	// create the file
 	out, err := os.Create(filepath)
@@ -104,11 +113,22 @@ func (session *DefaultSession) tryDownloadFile(filepath string, uri string) erro
 	}
 
 	// additional validation to compare sent headers with the written file
-	err = session.CheckDownloadedFileForErrors(written, resp.Header)
+	err = s.CheckDownloadedFileForErrors(written, resp.Header)
 	return err
 }
 
 // retrieve the client
-func (session *DefaultSession) GetClient() *http.Client {
-	return session.Client
+func (s *DefaultSession) GetClient() *http.Client {
+	return s.Client
+}
+
+// wait for the leaky bucket to fill again
+func (s *DefaultSession) applyRateLimit() {
+	// if no rate limiter is defined we don't have to wait
+	if s.RateLimiter != nil {
+		// wait for request to stay within the rate limit
+		if err := s.RateLimiter.Wait(s.ctx); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
