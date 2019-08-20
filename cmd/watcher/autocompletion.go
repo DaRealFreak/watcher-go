ac275c6d1dbb1ed5b5ac52155e09285d933fc6b5
DaRealFreak <steffen.keuper@web.de>
2019-08-20 03:29:17 +0200

    [TASK] extract method for auto completion script creation and output

--- cmd/watcher/autocompletion.go
+++ cmd/watcher/autocompletion.go
@@ -1,14 +1,16 @@
 package watcher
 
 import (
+	"bytes"
 	"fmt"
-	"github.com/mitchellh/go-homedir"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/mitchellh/go-homedir"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
 )
 
 func (cli *CliApplication) addGenerateAutoCompletionCommand() {
@@ -32,22 +34,16 @@ func (cli *CliApplication) getGenerateAutoCompletionCommandPowerShell() *cobra.C
 		Use:   "powershell",
 		Short: "generates auto completion for PowerShell",
 		Run: func(cmd *cobra.Command, args []string) {
-			if dir, err := homedir.Dir(); err == nil {
-				if err := os.MkdirAll(filepath.Join(dir, ".watcher", "completion"), os.ModePerm); err != nil {
-					log.Fatal(err)
-				}
-				filePath := filepath.ToSlash(filepath.Join(dir, ".watcher", "completion", "autocomplete_powershell.ps1"))
-				if err := cli.rootCmd.GenPowerShellCompletionFile(filePath); err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println("auto completion script got created at: " + filePath)
-				fmt.Println(
-					fmt.Sprintf(
-						"run the following command to add the auto completion to your profile: \n%s",
-						"echo 'Import-Module -Name \""+filePath+"\"' >> $PROFILE",
-					),
-				)
-			} else {
+			buffer := new(bytes.Buffer)
+			if err := cli.rootCmd.GenPowerShellCompletion(buffer); err != nil {
+				log.Fatal(err)
+			}
+
+			if err := cli.createAutoCompletionFile(
+				"autocomplete_powershell.ps1",
+				buffer.Bytes(),
+				"echo 'Import-Module -Name \"%s\"' >> $PROFILE",
+			); err != nil {
 				log.Fatal(err)
 			}
 		},
@@ -60,24 +56,19 @@ func (cli *CliApplication) getGenerateAutoCompletionCommandBash() *cobra.Command
 		Use:   "bash",
 		Short: "generates auto completion for Bash",
 		Run: func(cmd *cobra.Command, args []string) {
-			if dir, err := homedir.Dir(); err == nil {
-				if err := os.MkdirAll(filepath.Join(dir, ".watcher", "completion"), os.ModePerm); err != nil {
-					log.Fatal(err)
-				}
-				filePath := filepath.ToSlash(filepath.Join(dir, ".watcher", "completion", "autocomplete_bash.sh"))
-				if err := cli.rootCmd.GenBashCompletionFile(filePath); err != nil {
-					log.Fatal(err)
-				}
-				// update path to current executable to work with .exe instead of just without extension
-				cli.updateBashCompletionCommandToCurrentExecutable(filePath)
-				fmt.Println("auto completion script got created at: " + filePath)
-				fmt.Println(
-					fmt.Sprintf(
-						"run the following command to add the auto completion to your profile: \n%s",
-						"printf '\\nsource \""+filePath+"\"' >> ~/.bashrc",
-					),
-				)
-			} else {
+			buffer := new(bytes.Buffer)
+			if err := cli.rootCmd.GenBashCompletion(buffer); err != nil {
+				log.Fatal(err)
+			}
+
+			// update path to current executable to work with .exe instead of just without extension
+			fContent := cli.updateBashCompletionCommandToCurrentExecutable(buffer.Bytes())
+
+			if err := cli.createAutoCompletionFile(
+				"autocomplete_bash.sh",
+				fContent,
+				"printf '\\nsource \"%s\"' >> ~/.bashrc",
+			); err != nil {
 				log.Fatal(err)
 			}
 		},
@@ -90,22 +81,16 @@ func (cli *CliApplication) getGenerateAutoCompletionCommandZsh() *cobra.Command 
 		Use:   "zsh",
 		Short: "generates auto completion for Zsh",
 		Run: func(cmd *cobra.Command, args []string) {
-			if dir, err := homedir.Dir(); err == nil {
-				if err := os.MkdirAll(filepath.Join(dir, ".watcher", "completion"), os.ModePerm); err != nil {
-					log.Fatal(err)
-				}
-				filePath := filepath.ToSlash(filepath.Join(dir, ".watcher", "completion", "autocomplete_zsh.sh"))
-				if err := cli.rootCmd.GenBashCompletionFile(filePath); err != nil {
-					log.Fatal(err)
-				}
-				fmt.Println("auto completion script got created at: " + filePath)
-				fmt.Print(
-					fmt.Sprintf(
-						"run the following command to add the auto completion to your profile: \n%s",
-						"printf '\\nsh \""+filePath+"\"' >> ~/.zshrc",
-					),
-				)
-			} else {
+			buffer := new(bytes.Buffer)
+			if err := cli.rootCmd.GenZshCompletion(buffer); err != nil {
+				log.Fatal(err)
+			}
+
+			if err := cli.createAutoCompletionFile(
+				"autocomplete_zsh.sh",
+				buffer.Bytes(),
+				"printf '\\nsh \"%s\"' >> ~/.zshrc",
+			); err != nil {
 				log.Fatal(err)
 			}
 		},
@@ -113,13 +98,29 @@ func (cli *CliApplication) getGenerateAutoCompletionCommandZsh() *cobra.Command 
 	return zshCmd
 }
 
-func (cli *CliApplication) updateBashCompletionCommandToCurrentExecutable(filePath string) {
+func (cli *CliApplication) createAutoCompletionFile(fName string, fContent []byte, activationCmd string) (err error) {
+	if dir, err := homedir.Dir(); err == nil {
+		if err := os.MkdirAll(filepath.Join(dir, ".watcher", "completion"), os.ModePerm); err != nil {
+			return err
+		}
+		filePath := filepath.ToSlash(filepath.Join(dir, ".watcher", "completion", fName))
+		if err := ioutil.WriteFile(filePath, fContent, os.ModePerm); err != nil {
+			return err
+		}
+		fmt.Println("auto completion script got created at: " + filePath)
+		fmt.Print(
+			fmt.Sprintf(
+				"run the following command to add the auto completion to your profile: \n%s",
+				fmt.Sprintf(activationCmd, filePath),
+			),
+		)
+	}
+	return err
+}
+
+func (cli *CliApplication) updateBashCompletionCommandToCurrentExecutable(fileContent []byte) []byte {
 	if executable, err := os.Executable(); err == nil {
 		executable = filepath.Base(executable)
-		fileContent, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			log.Fatal(err)
-		}
 
 		newContents := strings.Replace(
 			string(fileContent),
@@ -128,8 +129,7 @@ func (cli *CliApplication) updateBashCompletionCommandToCurrentExecutable(filePa
 			-1,
 		)
 
-		if err := ioutil.WriteFile(filePath, []byte(newContents), 0); err != nil {
-			log.Fatal(err)
-		}
+		return []byte(newContents)
 	}
+	return fileContent
 }
