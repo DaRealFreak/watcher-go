025984333afa86fa1bd83acaadfa63ac68af0028
Steffen Keuper <steffen.keuper@web.de>
2019-08-21 14:49:47 +0200

    [TASK] add option to run all modules parallel to each other, fixes #7

--- cmd/watcher/run.go
+++ cmd/watcher/run.go
@@ -9,28 +9,36 @@ import (
 func (cli *CliApplication) addRunCommand() {
 	var downloadDirectory string
 	var moduleURL string
+	var runParallel bool
 	// runs the main functionality to update all tracked items
 	var runCmd = &cobra.Command{
 		Use:   "run",
 		Short: "update all tracked items",
 		Run: func(cmd *cobra.Command, args []string) {
-			cli.watcher.Run(moduleURL)
+			cli.watcher.Run(moduleURL, runParallel)
 		},
 	}
-	runCmd.PersistentFlags().StringVarP(
+	runCmd.Flags().StringVarP(
 		&downloadDirectory,
 		"directory",
 		"d",
 		"",
 		"download directory (will be saved in config file)",
 	)
-	runCmd.PersistentFlags().StringVarP(
+	runCmd.Flags().StringVarP(
 		&moduleURL,
 		"url",
 		"u",
 		"",
 		"url of module you want to run",
 	)
-	_ = viper.BindPFlag("download.directory", runCmd.PersistentFlags().Lookup("directory"))
+	runCmd.Flags().BoolVarP(
+		&runParallel,
+		"parallel",
+		"p",
+		false,
+		"run modules parallel",
+	)
+	_ = viper.BindPFlag("download.directory", runCmd.Flags().Lookup("directory"))
 	cli.rootCmd.AddCommand(runCmd)
 }
--- pkg/watcher/main.go
+++ pkg/watcher/main.go
@@ -2,8 +2,8 @@ package watcher
 
 import (
 	"fmt"
-
 	"github.com/DaRealFreak/watcher-go/pkg/raven"
+	"sync"
 
 	"github.com/DaRealFreak/watcher-go/pkg/database"
 	"github.com/DaRealFreak/watcher-go/pkg/models"
@@ -27,8 +27,8 @@ func NewWatcher() *Watcher {
 	return &watcher
 }
 
-// Run is the main functionality, updates all tracked items
-func (app *Watcher) Run(moduleURL string) {
+// Run is the main functionality, updates all tracked items either parallel or linear
+func (app *Watcher) Run(moduleURL string, parallel bool) {
 	var trackedItems []*models.TrackedItem
 	if moduleURL != "" {
 		module := app.ModuleFactory.GetModuleFromURI(moduleURL)
@@ -36,11 +36,39 @@ func (app *Watcher) Run(moduleURL string) {
 	} else {
 		trackedItems = app.DbCon.GetTrackedItems(nil, false)
 	}
-	for _, item := range trackedItems {
-		module := app.ModuleFactory.GetModule(item.Module)
-		if !module.IsLoggedIn() {
-			app.loginToModule(module)
+
+	if parallel {
+		groupedItems := make(map[string][]*models.TrackedItem)
+		for _, item := range trackedItems {
+			groupedItems[item.Module] = append(groupedItems[item.Module], item)
 		}
+		var wg sync.WaitGroup
+		wg.Add(len(groupedItems))
+		for moduleKey, items := range groupedItems {
+			go app.runForItems(moduleKey, items, &wg)
+		}
+		wg.Wait()
+	} else {
+		for _, item := range trackedItems {
+			module := app.ModuleFactory.GetModule(item.Module)
+			if !module.IsLoggedIn() {
+				app.loginToModule(module)
+			}
+			log.Info(fmt.Sprintf("parsing item %s (current id: %s)", item.URI, item.CurrentItem))
+			module.Parse(item)
+		}
+	}
+}
+
+// runForItems is the go routine to parse run parallel for groups
+func (app *Watcher) runForItems(moduleKey string, trackedItems []*models.TrackedItem, wg *sync.WaitGroup) {
+	defer wg.Done()
+	module := app.ModuleFactory.GetModule(moduleKey)
+	if !module.IsLoggedIn() {
+		app.loginToModule(module)
+	}
+
+	for _, item := range trackedItems {
 		log.Info(fmt.Sprintf("parsing item %s (current id: %s)", item.URI, item.CurrentItem))
 		module.Parse(item)
 	}
