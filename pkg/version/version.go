1a43f0a6691e0d265fd005c45887e6b6bb123586
DaRealFreak <steffen.keuper@web.de>
2019-08-25 16:43:42 +0200

    [TASK] bump version to 1.0.10

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // VERSION program version
-const VERSION = "1.0.9"
+const VERSION = "1.0.10"
 
 // RepositoryURL repository url to check for new versions
 const RepositoryURL = "DaRealFreak/watcher-go"
