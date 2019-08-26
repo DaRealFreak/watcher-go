b8390e1adc5c0e241829ed617f8780a70376ff9b
Steffen Keuper <steffen.keuper@web.de>
2019-08-26 14:18:23 +0200

    [TASK] add test utility for archive readers

---
+++ pkg/archive/archivetest/reader.go
@@ -0,0 +1,50 @@
+package archivetest
+
+import (
+	"github.com/DaRealFreak/watcher-go/pkg/archive"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
+
+// GetFiles checks if all added files are contained in the archive
+func GetFiles(t *testing.T, reader archive.Reader) {
+	assertion := assert.New(t)
+
+	archiveFiles, err := reader.GetFiles()
+	assertion.NoError(err)
+	assertion.Equal(len(GetSharedTestFiles()), len(archiveFiles))
+}
+
+// GetFile tests if all generated files can get retrieved from the generated archive
+func GetFile(t *testing.T, reader archive.Reader) {
+	assertion := assert.New(t)
+	for fileName, fileContent := range GetSharedTestFiles() {
+		reader, err := reader.GetFile(fileName)
+		assertion.NoError(err)
+		content, err := ioutil.ReadAll(reader)
+		assertion.NoError(err)
+		assertion.Equal(content, []byte(fileContent))
+	}
+}
+
+// GenerateTestFiles generates a new archive with the passed files
+func GenerateTestFiles(t *testing.T, writer archive.Writer) {
+	var assertion = assert.New(t)
+	for fileName, fileContent := range GetSharedTestFiles() {
+		_, err := writer.AddFile(fileName, []byte(fileContent))
+		assertion.NoError(err)
+	}
+	err := writer.Close()
+	assertion.NoError(err)
+}
+
+// GetSharedTestFiles returns the shared test files/file content for the unit tests
+func GetSharedTestFiles() map[string]string {
+	return map[string]string{
+		"accounts.sql":        "INSERT INTO test; COMMIT;",
+		"README":              "some meaningful content",
+		"content_dir/LICENSE": "license content",
+		"empty_dir/":          "",
+	}
+}
