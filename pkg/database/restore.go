153f26f7280668131723bdd704f583601ef15f54
DaRealFreak <steffen.keuper@web.de>
2019-08-27 00:13:05 +0200

    [TASK] add nosec to RestoreTableFromFile since the database path is user controlled and should also be able to be a non existing file

--- pkg/database/restore.go
+++ pkg/database/restore.go
@@ -1,11 +1,13 @@
 package database
 
 import (
-	"github.com/spf13/viper"
 	"os/exec"
+
+	"github.com/spf13/viper"
 )
 
 // RestoreTableFromFile executes the passed SQL file, used primarily for restoration
+// #nosec
 func (db *DbIO) RestoreTableFromFile(fileName string) error {
 	return exec.Command("sqlite3", viper.GetString("Database.Path"), ".read '"+fileName+"'").Run()
 }
