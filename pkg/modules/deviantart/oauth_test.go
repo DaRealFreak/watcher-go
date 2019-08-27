c10901db75bb7f5111e2225d31370e6999f99ff3
DaRealFreak <steffen.keuper@web.de>
2019-08-27 04:44:54 +0200

    [TASK] implement test cases for OAuth2 token retrieval

--- pkg/modules/deviantart/oauth_test.go
+++ pkg/modules/deviantart/oauth_test.go
@@ -1,18 +1,43 @@
 package main
 
 import (
+	"net/http"
+	"sync"
 	"testing"
 
-	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-// main function for testing
+// TestRetrieveOAuth2Code tests the local http server to retrieve the OAuth2 code
 func TestRetrieveOAuth2Code(t *testing.T) {
-	_ = assert.New(t)
+	assertion := assert.New(t)
 
-	// ToDo: both cases:
-	//  - calling http://lvh.me:8080/cb?code=test_code to retrieve "test_code"
-	//  - timing out the function (60 seconds) to retrieve empty string
-	log.Infof("deviantart oauth2 granted: %s", retrieveOAuth2Code())
+	var wg sync.WaitGroup
+	wg.Add(1)
+	// routine this check so we can actually call the request
+	go checkNoTimeout(t, &wg)
+	// call local host resolved domain with test_code as code
+	_, err := http.Get("http://lvh.me:8080/cb?code=test_code")
+	assertion.NoError(err)
+	// wait for this test to finish
+	wg.Wait()
+
+	// wait for timeout returning empty string
+	oAuth2Code := retrieveOAuth2Code()
+	assertion.Equal(oAuth2Code, "")
+}
+
+// checkNoTimeout checks the web server response if actually called
+// to test this we have to goroutine this check and lock it to prevent overlapping port bindings
+func checkNoTimeout(t *testing.T, wg *sync.WaitGroup) {
+	assertion := assert.New(t)
+	ch := make(chan string)
+	// listen with a go routine to be able to time it out
+	go func() {
+		ch <- retrieveOAuth2Code()
+	}()
+
+	receivedCode := <-ch
+	assertion.Equal(receivedCode, "test_code")
+	wg.Done()
 }
