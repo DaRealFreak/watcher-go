9bbe004e9cb99dd7397d848ee34a97d6c45e5fb1
DaRealFreak <steffen.keuper@web.de>
2019-08-29 05:05:35 +0200

    [TASK] add scope constant to endpoint

--- pkg/modules/deviantart/endpoint_util.go
+++ pkg/modules/deviantart/endpoint_util.go
@@ -8,17 +8,20 @@ import (
 	"github.com/DaRealFreak/watcher-go/pkg/raven"
 )
 
+// ScopeUtil is the required scope for the OAuth2 Token
+const ScopeUtil = "basic"
+
 // Placebo implements the API endpoint https://www.deviantart.com/api/v1/oauth2/placebo
-func (m *deviantArt) Placebo() (response *PlaceboResponse) {
-	var placebo PlaceboResponse
+func (m *deviantArt) Placebo() (response *UtilPlaceboResponse) {
+	var placebo UtilPlaceboResponse
 	values := url.Values{}
-	res, err := m.deviantArtSession.APIPost("https://www.deviantart.com/api/v1/oauth2/placebo", values)
+	res, err := m.deviantArtSession.APIPost("https://www.deviantart.com/api/v1/oauth2/placebo", values, ScopeUtil)
 	raven.CheckError(err)
 
 	content, err := ioutil.ReadAll(res.Body)
 	raven.CheckError(err)
 
-	// unmarshal the request content into the PlaceboResponse
+	// unmarshal the request content into the UtilPlaceboResponse
 	raven.CheckError(json.Unmarshal(content, &placebo))
 	return &placebo
 }
