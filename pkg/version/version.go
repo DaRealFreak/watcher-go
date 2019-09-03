a23bc16a20bf4a129003dff063e46f0dba88f05a
Steffen Keuper <steffen.keuper@web.de>
2019-09-03 12:55:30 +0200

    [TASK] bump version to 1.1.3

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // VERSION program version
-const VERSION = "1.1.2"
+const VERSION = "1.1.3"
 
 // RepositoryURL repository url to check for new versions
 const RepositoryURL = "DaRealFreak/watcher-go"
