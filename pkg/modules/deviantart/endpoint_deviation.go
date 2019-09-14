63eeb8b9139a82c1bbbd961675e4b7e16cb2a764
DaRealFreak <steffen.keuper@web.de>
2019-09-14 12:39:34 +0200

    [TASK] implement fallback solution for the download API endpoint since it returns a lot of internal server responses, relates to #20

--- pkg/modules/deviantart/endpoint_deviation.go
+++ pkg/modules/deviantart/endpoint_deviation.go
@@ -1,6 +1,8 @@
 package deviantart
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/url"
 	"strconv"
 
@@ -48,6 +50,31 @@ func (m *deviantArt) DeviationDownload(deviationID string) (apiRes *Image, apiEr
 	// map the http.Response into either the api response or the api error
 	m.mapAPIResponse(res, &apiRes, &apiErr)
 	return apiRes, apiErr
+}
+
+// DeviationDownloadFallback is a fallback solution for the API endpoint
+// https://www.deviantart.com/api/v1/oauth2/deviation/download/{deviationid}
+// since the endpoint returns a lot of internal server error responses, while the web interface works properly
+func (m *deviantArt) DeviationDownloadFallback(deviationURL string) (apiRes *Image, err error) {
+	res, err := m.deviantArtSession.Get(deviationURL)
+	raven.CheckError(err)
+
+	doc := m.Session.GetDocument(res)
+	dl := doc.Find("a[href*=\"https://www.deviantart.com/download/\"]").First()
+	dlLink, exists := dl.Attr("href")
+	if dl.Length() != 1 || !exists {
+		return nil, fmt.Errorf("no download link found in deviation URL: %s", deviationURL)
+	}
+	dlWidth, _ := dl.Attr("data-download_width")
+	dlHeight, _ := dl.Attr("data-download_height")
+
+	// we can't set Transparency and FileSize without downloading/requesting the file, so we don't return that here
+	// it is only a fallback solution after all
+	return &Image{
+		Src:    dlLink,
+		Width:  json.Number(dlWidth),
+		Height: json.Number(dlHeight),
+	}, nil
 }
 
 // DeviationEmbeddedContent implements the API endpoint
