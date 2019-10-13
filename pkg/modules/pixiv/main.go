66da419cd10620ef9e82ea5aa7b400d4e4c4a574
DaRealFreak <steffen.keuper@web.de>
2019-10-14 01:13:09 +0200

    [BUGFIX] fix condition of setting complete status to true for single illustrations

--- pkg/modules/pixiv/main.go
+++ pkg/modules/pixiv/main.go
@@ -152,13 +152,13 @@ func (m *pixiv) Login(account *models.Account) bool {
 func (m *pixiv) Parse(item *models.TrackedItem) error {
 	switch {
 	case strings.Contains(item.URI, "illust_id=") || strings.Contains(item.URI, "artworks"):
-		return m.parseUserIllustration(item)
-	case strings.Contains(item.URI, "/member.php") || strings.Contains(item.URI, "/member_illust.php"):
-		err := m.parseUserIllustrations(item)
-		if err != nil {
+		err := m.parseUserIllustration(item)
+		if err == nil {
 			m.DbIO.ChangeTrackedItemCompleteStatus(item, true)
 		}
 		return err
+	case strings.Contains(item.URI, "/member.php") || strings.Contains(item.URI, "/member_illust.php"):
+		return m.parseUserIllustrations(item)
 	}
 	return nil
 }
