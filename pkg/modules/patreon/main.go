69cc7450a91090cd77a3050b1450ea7c23083ded
Steffen Keuper <steffen.keuper@web.de>
2019-11-20 14:01:42 +0100

    [TASK] adding base for patreon module, related to #21

---
+++ pkg/modules/patreon/main.go
@@ -0,0 +1,70 @@
+// Package patreon contains the implementation of the patreon module
+package patreon
+
+import (
+	formatter "github.com/DaRealFreak/colored-nested-formatter"
+	"github.com/DaRealFreak/watcher-go/pkg/http/session"
+	"github.com/DaRealFreak/watcher-go/pkg/models"
+	"github.com/DaRealFreak/watcher-go/pkg/modules"
+	"github.com/DaRealFreak/watcher-go/pkg/raven"
+	"github.com/spf13/cobra"
+	"regexp"
+)
+
+// patreon contains the implementation of the ModuleInterface
+type patreon struct {
+	*models.Module
+}
+
+// nolint: gochecknoinits
+// init function registers the bare and the normal module to the module factories
+func init() {
+	modules.GetModuleFactory().RegisterModule(NewBareModule())
+}
+
+// NewBareModule returns a bare module implementation for the CLI options
+func NewBareModule() *models.Module {
+	module := &models.Module{
+		Key:           "patreon.com",
+		RequiresLogin: true,
+		LoggedIn:      false,
+		UriSchemas: []*regexp.Regexp{
+			regexp.MustCompile("www.patreon.com"),
+		},
+	}
+	module.ModuleInterface = &patreon{
+		Module: module,
+	}
+
+	// register module to log formatter
+	formatter.AddFieldMatchColorScheme("module", &formatter.FieldMatch{
+		Value: module.Key,
+		Color: "232:167",
+	})
+
+	return module
+}
+
+// InitializeModule initializes the module
+func (m *patreon) InitializeModule() {
+	// set the module implementation for access to the session, database, etc
+	m.Session = session.NewSession(m.Key)
+
+	// set the proxy if requested
+	raven.CheckError(m.Session.SetProxy(m.GetProxySettings()))
+}
+
+// AddSettingsCommand adds custom module specific settings and commands to our application
+func (m *patreon) AddSettingsCommand(command *cobra.Command) {
+	m.AddProxyCommands(command)
+}
+
+// Login logs us in for the current session if possible/account available
+func (m *patreon) Login(account *models.Account) bool {
+	return m.LoggedIn
+}
+
+// Parse parses the tracked item
+func (m *patreon) Parse(item *models.TrackedItem) error {
+	return nil
+}
