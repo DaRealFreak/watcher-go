f53580d93833e9887fac7fe7722fd05bc9c4cbc5
Steffen Keuper <steffen.keuper@web.de>
2019-11-27 15:33:02 +0100

    [TASK] add function to retrieve creator id from user page

---
+++ pkg/modules/patreon/user.go
@@ -0,0 +1,22 @@
+package patreon
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// getCreatorID extracts the creator ID from the campaign URI
+func (m *patreon) getCreatorID(campaignURI string) (int, error) {
+	res, err := m.Session.Get(campaignURI)
+	if err != nil {
+		return 0, err
+	}
+
+	creatorIDMatches := m.creatorIPattern.FindStringSubmatch(m.Session.GetDocument(res).Text())
+	if len(creatorIDMatches) != 2 {
+		return 0, fmt.Errorf("unexpected amount of matches in search of creator id ")
+	}
+
+	creatorID, _ := strconv.ParseInt(creatorIDMatches[1], 10, 64)
+	return int(creatorID), nil
+}
