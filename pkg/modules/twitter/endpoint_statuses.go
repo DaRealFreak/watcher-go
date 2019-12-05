14418fac0fa9bd61fdc855cf5289b293c4669e08
Steffen Keuper <steffen.keuper@web.de>
2019-12-05 15:20:22 +0100

    [TASK] add structs for entities including media elements and the retweeted status

--- pkg/modules/twitter/endpoint_statuses.go
+++ pkg/modules/twitter/endpoint_statuses.go
@@ -10,6 +10,15 @@ type Tweet struct {
 	CreatedAt string      `json:"created_at"`
 	ID        json.Number `json:"id"`
 	Text      string      `json:"text"`
+	Entities  struct {
+		MediaElement []*struct {
+			ID            json.Number `json:"id"`
+			MediaURLHTTPS string      `json:"media_url_https"`
+			DisplayURL    string      `json:"display_url"`
+			Type          string      `json:"type"`
+		} `json:"media"`
+	} `json:"entities"`
+	RetweetedStatus interface{} `json:"retweeted_status"`
 }
 
 func (m *twitter) getUserTimeline(values url.Values) (apiRes []*Tweet, apiErr *APIError, err error) {
