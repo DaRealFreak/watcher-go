ecb00e933d1cff1e40516ddaa6560d5ae91a2c50
Steffen Keuper <steffen.keuper@web.de>
2019-12-05 13:45:40 +0100

    [TASK] implement user timeline functionality of statuses endpoint

---
+++ pkg/modules/twitter/endpoint_statuses.go
@@ -0,0 +1,30 @@
+package twitter
+
+import (
+	"encoding/json"
+	"net/url"
+)
+
+// Tweet contains the struct to unmarshal twitters API responses
+type Tweet struct {
+	CreatedAt string      `json:"created_at"`
+	ID        json.Number `json:"id"`
+	Text      string      `json:"text"`
+}
+
+func (m *twitter) getUserTimeline(values url.Values) (apiRes []*Tweet, apiErr *APIError, err error) {
+	apiURI := "https://api.twitter.com/1.1/statuses/user_timeline.json"
+	if values.Encode() != "" {
+		apiURI += "?" + values.Encode()
+	}
+
+	res, err := m.Session.Get(apiURI)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// map the http.Response into either the api response or the api error
+	err = m.mapAPIResponse(res, &apiRes, &apiErr)
+
+	return apiRes, apiErr, err
+}
