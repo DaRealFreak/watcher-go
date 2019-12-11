daf19b4024e333be1e882bd09c7b0555712746f3
Steffen Keuper <steffen.keuper@web.de>
2019-12-11 15:56:09 +0100

    [TASK] implement post information endpoint

--- pkg/modules/pixiv/ajax_api/api_test.go
+++ pkg/modules/pixiv/ajax_api/api_test.go
@@ -20,11 +20,18 @@ func TestLogin(t *testing.T) {
 	assert.New(t).NoError(err)
 	assert.New(t).NotNil(creatorInfo)
 
-	postInfo, err := ajaxAPI.GetPostList(12345)
-	assert.New(t).NoError(err)
-	assert.New(t).NotNil(postInfo)
-
 	nextPagePostInfo, err := ajaxAPI.GetPostListByURL(creatorInfo.Body.Post.NextURL)
 	assert.New(t).NoError(err)
 	assert.New(t).NotNil(nextPagePostInfo)
+
+	postList, err := ajaxAPI.GetPostList(12345)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(postList)
+
+	if len(postList.Body.Items) > 0 {
+		postID, _ := postList.Body.Items[0].ID.Int64()
+		postInfo, err := ajaxAPI.GetPostInfo(int(postID))
+		assert.New(t).NoError(err)
+		assert.New(t).NotNil(postInfo)
+	}
 }
