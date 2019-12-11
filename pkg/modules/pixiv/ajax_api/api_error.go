9edf8ed0bfdb3267f10b3fbd0dc74f7240397565
Steffen Keuper <steffen.keuper@web.de>
2019-12-11 14:20:00 +0100

    [TASK] differentiate between API error responses and API request error responses

--- pkg/modules/pixiv/ajax_api/api_error.go
+++ pkg/modules/pixiv/ajax_api/api_error.go
@@ -2,10 +2,21 @@ package ajaxapi
 
 // APIError contains error messages of the API
 type APIError struct {
+	ErrorOccurred bool   `json:"error"`
+	ErrorMessage  string `json:"message"`
+}
+
+// APIRequestError is the error struct of invalid requests
+type APIRequestError struct {
 	ErrorMessage string `json:"error"`
 }
 
 // Error returns the occurred API error message
 func (e APIError) Error() string {
+	return e.ErrorMessage
+}
+
+// Error returns the occurred API error message
+func (e APIRequestError) Error() string {
 	return e.ErrorMessage
 }
