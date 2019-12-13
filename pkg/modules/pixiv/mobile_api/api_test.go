571a03f509a452647d3bd527b7e32c7318628af9
Steffen Keuper <steffen.keuper@web.de>
2019-12-13 17:23:00 +0100

    [TASK] remove illustration test from api test

--- pkg/modules/pixiv/mobile_api/api_test.go
+++ pkg/modules/pixiv/mobile_api/api_test.go
@@ -1,13 +1,11 @@
-package mobile_api
+package mobileapi
 
 import (
-	"fmt"
-	"github.com/DaRealFreak/watcher-go/pkg/models"
-	"github.com/stretchr/testify/assert"
-	"io/ioutil"
-	"net/url"
 	"os"
 	"testing"
+
+	"github.com/DaRealFreak/watcher-go/pkg/models"
+	"github.com/stretchr/testify/assert"
 )
 
 func getTestMobileAPI() *MobileAPI {
@@ -19,23 +17,6 @@ func getTestMobileAPI() *MobileAPI {
 	mobileAPI, _ := NewMobileAPI("pixiv Mobile API", testAccount)
 
 	return mobileAPI
-}
-
-func TestNewMobileAPI(t *testing.T) {
-	mobileAPI := getTestMobileAPI()
-
-	apiURL, _ := url.Parse("https://app-api.pixiv.net/v1/illust/detail")
-	data := url.Values{
-		"illust_id": {"123456"},
-	}
-	apiURL.RawQuery = data.Encode()
-
-	res, err := mobileAPI.Session.Get(apiURL.String())
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(ioutil.ReadAll(res.Body))
 }
 
 func TestLogin(t *testing.T) {
