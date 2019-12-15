8b66258ce71f220ec9334f1a94a490536c50c4d4
DaRealFreak <steffen.keuper@web.de>
2019-12-15 22:25:25 +0100

    [TASK] implement search endpoint

---
+++ pkg/modules/pixiv/mobile_api/endpoint_search_test.go
@@ -0,0 +1,36 @@
+package mobileapi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMobileAPI_GetSearchIllust(t *testing.T) {
+	mobileAPI := getTestMobileAPI()
+
+	illustSearchResults, err := mobileAPI.GetSearchIllust(
+		"test", SearchModePartialTagMatch, SearchOrderDateDescending, 0,
+	)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(illustSearchResults)
+	assert.New(t).Equal(len(illustSearchResults.Illustrations), 30)
+	assert.New(t).NotEmpty(illustSearchResults.NextURL)
+}
+
+func TestMobileAPI_GetSearchIllustByURL(t *testing.T) {
+	mobileAPI := getTestMobileAPI()
+
+	illustSearchResults, err := mobileAPI.GetSearchIllust(
+		"test", SearchModePartialTagMatch, SearchOrderDateDescending, 0,
+	)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(illustSearchResults)
+	assert.New(t).Equal(len(illustSearchResults.Illustrations), 30)
+	assert.New(t).NotEmpty(illustSearchResults.NextURL)
+
+	nextPageIllustSearchResults, err := mobileAPI.GetSearchIllustByURL(illustSearchResults.NextURL)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(nextPageIllustSearchResults)
+	assert.New(t).Equal(len(nextPageIllustSearchResults.Illustrations), 30)
+}
