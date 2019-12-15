2879e6818ec9984a0e6203620034ee48578a71c0
DaRealFreak <steffen.keuper@web.de>
2019-12-15 17:39:06 +0100

    [TASK] use an existing fanbox for the unit tests, limit the posts to 50

--- pkg/modules/pixiv/ajax_api/endpoint_creator_test.go
+++ pkg/modules/pixiv/ajax_api/endpoint_creator_test.go
@@ -7,20 +7,20 @@ import (
 )
 
 func TestAjaxAPI_GetCreator(t *testing.T) {
-	creatorInfo, err := getTestAjaxAPI().GetCreator(12345)
+	creatorInfo, err := getTestAjaxAPI().GetCreator(8189060)
 	assert.New(t).NoError(err)
 	assert.New(t).NotNil(creatorInfo)
 }
 
 func TestAjaxAPI_GetPostList(t *testing.T) {
-	postList, err := getTestAjaxAPI().GetPostList(12345)
+	postList, err := getTestAjaxAPI().GetPostList(8189060, 50)
 	assert.New(t).NoError(err)
 	assert.New(t).NotNil(postList)
 }
 
 func TestAjaxAPI_GetPostListByURL(t *testing.T) {
 	// retrieve next URL from previous Post List (we are using 200 items as limit, we need a creator with > 200 items)
-	postList, err := getTestAjaxAPI().GetPostList(12345)
+	postList, err := getTestAjaxAPI().GetPostList(8189060, 50)
 	assert.New(t).NoError(err)
 	assert.New(t).NotNil(postList)
 	assert.New(t).NotEmpty(postList.Body.NextURL)
@@ -30,7 +30,7 @@ func TestAjaxAPI_GetPostListByURL(t *testing.T) {
 	assert.New(t).NotNil(nextPagePostList)
 
 	// retrieve next URL from creator list
-	creatorInfo, err := getTestAjaxAPI().GetCreator(12345)
+	creatorInfo, err := getTestAjaxAPI().GetCreator(8189060)
 	assert.New(t).NoError(err)
 	assert.New(t).NotNil(creatorInfo)
 	assert.New(t).NotEmpty(creatorInfo.Body.Post.NextURL)
