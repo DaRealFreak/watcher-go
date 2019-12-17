7694e58bffdc6cdacfe08c336996ec0a2f6b3772
Steffen Keuper <steffen.keuper@web.de>
2019-12-17 18:53:35 +0100

    [TASK] implement unit tests for user endpoint illusts function

--- pkg/modules/pixiv/mobile_api/endpoint_user_test.go
+++ pkg/modules/pixiv/mobile_api/endpoint_user_test.go
@@ -13,3 +13,28 @@ func TestMobileAPI_GetUserDetail(t *testing.T) {
 	assert.New(t).NoError(err)
 	assert.New(t).NotNil(userDetail)
 }
+
+func TestMobileAPI_GetUserIllusts(t *testing.T) {
+	mobileAPI := getTestMobileAPI()
+
+	userIllusts, err := mobileAPI.GetUserIllusts(7210261, "", 0)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(userIllusts)
+	assert.New(t).Equal(len(userIllusts.Illustrations), 30)
+	assert.New(t).NotEmpty(userIllusts.NextURL)
+}
+
+func TestMobileAPI_GetUserIllustsByURL(t *testing.T) {
+	mobileAPI := getTestMobileAPI()
+
+	userIllusts, err := mobileAPI.GetUserIllusts(7210261, "", 0)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(userIllusts)
+	assert.New(t).Equal(len(userIllusts.Illustrations), 30)
+	assert.New(t).NotEmpty(userIllusts.NextURL)
+
+	nextPageUserIllusts, err := mobileAPI.GetUserIllustsByURL(userIllusts.NextURL)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(nextPageUserIllusts)
+	assert.New(t).Equal(len(nextPageUserIllusts.Illustrations), 30)
+}
