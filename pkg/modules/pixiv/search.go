bda7914405b9dc6424c9dc83dc033729e35347db
Steffen Keuper <steffen.keuper@web.de>
2019-12-17 17:02:27 +0100

    [TASK] unescape search query before passing it to the API functionality, implement mobile API search

--- pkg/modules/pixiv/search.go
+++ pkg/modules/pixiv/search.go
@@ -8,14 +8,73 @@ import (
 	"github.com/DaRealFreak/watcher-go/pkg/models"
 	mobileapi "github.com/DaRealFreak/watcher-go/pkg/modules/pixiv/mobile_api"
 	publicapi "github.com/DaRealFreak/watcher-go/pkg/modules/pixiv/public_api"
+	log "github.com/sirupsen/logrus"
 )
 
 func (m *pixiv) parseSearch(item *models.TrackedItem) error {
-	return nil
+	searchWord := m.patterns.searchPattern.FindStringSubmatch(item.URI)[1]
+	searchWord, _ = url.QueryUnescape(searchWord)
+
+	searchMode, err := m.getSearchModeFromURI(item.URI)
+	if err != nil {
+		return err
+	}
+
+	var downloadQueue []*downloadQueueItem
+
+	currentItemID, _ := strconv.ParseInt(item.CurrentItem, 10, 64)
+	foundCurrentItem := false
+
+	response, err := m.mobileAPI.GetSearchIllust(searchWord, searchMode, mobileapi.SearchOrderDateDescending, 0)
+	if err != nil {
+		return err
+	}
+
+	for !foundCurrentItem {
+		for _, illustration := range response.Illustrations {
+			if item.CurrentItem == "" || illustration.ID > int(currentItemID) {
+				downloadQueue = append(downloadQueue, &downloadQueueItem{
+					ItemID:       illustration.ID,
+					DownloadTag:  m.SanitizePath(searchWord, false),
+					DownloadItem: illustration,
+				})
+			} else {
+				foundCurrentItem = true
+				break
+			}
+		}
+
+		if response.NextURL == "" {
+			break
+		}
+
+		response, err = m.mobileAPI.GetSearchIllustByURL(response.NextURL)
+		if err != nil {
+			if err.Error() == `{"offset":["Offset must be no more than 5000"]}` {
+				log.WithField("module", m.Key).Warningf(
+					"search %s has more than 5000 results. The mobile API is limited to 5000 search results, "+
+						"if you want to download more than 5000 results consider switching to the public search API",
+					item.URI,
+				)
+
+				break
+			}
+
+			return err
+		}
+	}
+
+	// reverse download queue to download old items first
+	for i, j := 0, len(downloadQueue)-1; i < j; i, j = i+1, j-1 {
+		downloadQueue[i], downloadQueue[j] = downloadQueue[j], downloadQueue[i]
+	}
+
+	return m.processDownloadQueue(downloadQueue, item)
 }
 
 func (m *pixiv) parseSearchPublic(item *models.TrackedItem) error {
 	searchWord := m.patterns.searchPattern.FindStringSubmatch(item.URI)[1]
+	searchWord, _ = url.QueryUnescape(searchWord)
 
 	searchMode, err := m.getSearchModeFromURI(item.URI)
 	if err != nil {
