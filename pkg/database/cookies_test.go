bc6ef310b62cdffe4146faf1a1b0fbf34f2b6eb4
Steffen Keuper <steffen.keuper@web.de>
2019-12-18 12:10:13 +0100

    [TASK] add function to retrieve specific cookie by name and module

--- pkg/database/cookies_test.go
+++ pkg/database/cookies_test.go
@@ -2,18 +2,23 @@ package database
 
 import (
 	"fmt"
-	"github.com/DaRealFreak/watcher-go/pkg/models"
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	"github.com/DaRealFreak/watcher-go/pkg/models"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestDbIO_GetCookies(t *testing.T) {
+func seedCookiesTable(t *testing.T) {
 	futureTimestamp := time.Now().AddDate(1, 0, 0).Unix()
 	pastTimestamp := time.Now().AddDate(-1, 0, 0).Unix()
 	currentTimestamp := time.Now().Unix()
 
-	_, err := dbIO.connection.Exec(fmt.Sprintf(`
+	// truncate table
+	_, err := dbIO.connection.Exec(`DELETE FROM cookies WHERE uid;`)
+	assert.New(t).NoError(err)
+
+	_, err = dbIO.connection.Exec(fmt.Sprintf(`
 		INSERT INTO cookies (uid, name, value, expiration, module, disabled)
 		VALUES (1, 'future_cookie', 'test123456', '%d', 'test.module', 0);`,
 		futureTimestamp,
@@ -33,7 +38,24 @@ func TestDbIO_GetCookies(t *testing.T) {
 		pastTimestamp,
 	))
 	assert.New(t).NoError(err)
+}
+
+func TestDbIO_GetCookies(t *testing.T) {
+	seedCookiesTable(t)
 
 	cookies := dbIO.GetCookies(&models.Module{Key: "test.module"})
 	assert.New(t).Equal(1, len(cookies))
+}
+
+func TestDbIO_GetCookie(t *testing.T) {
+	seedCookiesTable(t)
+
+	cookie := dbIO.GetCookie("future_cookie", &models.Module{Key: "test.module"})
+	assert.New(t).NotNil(cookie)
+
+	cookie = dbIO.GetCookie("current_cookie", &models.Module{Key: "test.module"})
+	assert.New(t).Empty(cookie)
+
+	cookie = dbIO.GetCookie("expired_cookie", &models.Module{Key: "test.module"})
+	assert.New(t).Empty(cookie)
 }
