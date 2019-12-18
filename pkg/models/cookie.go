426640d6e1cc0b3da1d9c6ea5219ea7da874cc8e
Steffen Keuper <steffen.keuper@web.de>
2019-12-18 16:00:30 +0100

    [TASK] add function to display the NullTime as either empty string or RFC 882 formatted string

--- pkg/models/cookie.go
+++ pkg/models/cookie.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"time"
 )
 
 // Cookie contains all the required details to set cookies for domains
@@ -12,4 +13,13 @@ type Cookie struct {
 	Expiration sql.NullTime
 	Module     string
 	Disabled   bool
+}
+
+// GetDisplayExpirationDate returns an empty string if the date is null or else an RFC822 formatted string
+func (c *Cookie) GetDisplayExpirationDate() string {
+	if !c.Expiration.Valid {
+		return ""
+	}
+
+	return c.Expiration.Time.Format(time.RFC822)
 }
