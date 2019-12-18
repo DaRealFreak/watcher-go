8687802004731c5fab70a68ec8ade933da949bd1
Steffen Keuper <steffen.keuper@web.de>
2019-12-18 15:00:01 +0100

    [TASK] change format to DateTime once again, add GetFirstOrCreateCookie and CreateCookie functions, add functionality to retrieve NullTime object from passed string

--- pkg/database/cookies_test.go
+++ pkg/database/cookies_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
 	"testing"
 	"time"
@@ -10,32 +11,35 @@ import (
 )
 
 func seedCookiesTable(t *testing.T) {
-	futureTimestamp := time.Now().AddDate(1, 0, 0).Unix()
-	pastTimestamp := time.Now().AddDate(-1, 0, 0).Unix()
-	currentTimestamp := time.Now().Unix()
+	futureTimestamp := time.Now().AddDate(1, 0, 0)
+	pastTimestamp := time.Now().AddDate(-1, 0, 0)
+	currentTimestamp := time.Now()
 
 	// truncate table
 	_, err := dbIO.connection.Exec(`DELETE FROM cookies WHERE uid;`)
 	assert.New(t).NoError(err)
 
+	// nolint: gosec
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
 		INSERT INTO cookies (name, expiration, module)
-		VALUES ('future_cookie', '%d', 'test.module');`,
-		futureTimestamp,
+		VALUES ('future_cookie', '%s', 'test.module');`,
+		futureTimestamp.Format(time.RFC3339),
 	))
 	assert.New(t).NoError(err)
 
+	// nolint: gosec
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
 		INSERT INTO cookies (name, expiration, module)
-		VALUES ('current_cookie', '%d', 'test.module');`,
-		currentTimestamp,
+		VALUES ('current_cookie', '%s', 'test.module');`,
+		currentTimestamp.Format(time.RFC3339),
 	))
 	assert.New(t).NoError(err)
 
+	// nolint: gosec
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
 		INSERT INTO cookies (name, expiration, module)
-		VALUES ('expired_cookie', '%d', 'test.module');`,
-		pastTimestamp,
+		VALUES ('expired_cookie', '%s', 'test.module');`,
+		pastTimestamp.Format(time.RFC3339),
 	))
 	assert.New(t).NoError(err)
 
@@ -45,17 +49,19 @@ func seedCookiesTable(t *testing.T) {
 	)
 	assert.New(t).NoError(err)
 
+	// nolint: gosec
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
 		INSERT INTO cookies (name, expiration, module)
-		VALUES ('future_cookie_2', '%d', 'test.module.2');`,
-		futureTimestamp,
+		VALUES ('future_cookie_2', '%s', 'test.module.2');`,
+		futureTimestamp.Format(time.RFC3339),
 	))
 	assert.New(t).NoError(err)
 
+	// nolint: gosec
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
 		INSERT INTO cookies (name, expiration, module)
-		VALUES ('expired_cookie_2', '%d', 'test.module.2');`,
-		pastTimestamp,
+		VALUES ('expired_cookie_2', '%s', 'test.module.2');`,
+		pastTimestamp.Format(time.RFC3339),
 	))
 	assert.New(t).NoError(err)
 
@@ -64,7 +70,6 @@ func seedCookiesTable(t *testing.T) {
 		VALUES ('null_cookie', null, 'test.module.2');`,
 	)
 	assert.New(t).NoError(err)
-
 }
 
 func TestDbIO_GetCookie(t *testing.T) {
@@ -91,4 +96,59 @@ func TestDbIO_GetAllCookies(t *testing.T) {
 
 	cookies = dbIO.GetAllCookies(nil)
 	assert.New(t).Equal(4, len(cookies))
+}
+
+func TestDbIO_CreateCookie(t *testing.T) {
+	// truncate table
+	_, err := dbIO.connection.Exec(`DELETE FROM cookies WHERE uid;`)
+	assert.New(t).NoError(err)
+
+	cookies := dbIO.GetAllCookies(nil)
+	assert.New(t).Equal(0, len(cookies))
+
+	// create normal cookie
+	dbIO.CreateCookie("name", "value", sql.NullTime{
+		Time:  time.Now().AddDate(1, 0, 0),
+		Valid: true,
+	}, &models.Module{Key: "test.module"})
+
+	cookies = dbIO.GetAllCookies(&models.Module{Key: "test.module"})
+	assert.New(t).Equal(1, len(cookies))
+
+	// create null expiration cookie
+	dbIO.CreateCookie("name", "value", sql.NullTime{}, &models.Module{Key: "test.module"})
+
+	cookies = dbIO.GetAllCookies(&models.Module{Key: "test.module"})
+	assert.New(t).Equal(2, len(cookies))
+}
+
+func TestDbIO_GetFirstOrCreateCookie(t *testing.T) {
+	// truncate table
+	_, err := dbIO.connection.Exec(`DELETE FROM cookies WHERE uid;`)
+	assert.New(t).NoError(err)
+
+	cookies := dbIO.GetAllCookies(nil)
+	assert.New(t).Equal(0, len(cookies))
+
+	dbIO.GetFirstOrCreateCookie(
+		"test", "name", time.Now().Add(24*time.Hour).Format(time.RFC1123), &models.Module{Key: "test.module"},
+	)
+
+	cookies = dbIO.GetAllCookies(&models.Module{Key: "test.module"})
+	assert.New(t).Equal(1, len(cookies))
+}
+
+func TestDbIO_getNullTimeFromString(t *testing.T) {
+	validTestStrings := []string{
+		// FireFox copy result on cookie expiration attribute
+		`Expires:"Sat, 19 Dec 2020 17:16:43 GMT"`,
+		// Chrome copy result on cookie expiration attribute
+		"2054-12-31T23:59:59.284Z",
+		// Edge/IE has no direct copy functionality (lol), but displays it like this
+		"Wed, 18 Dec 2019 13:57:18 GMT",
+	}
+
+	for _, testString := range validTestStrings {
+		assert.New(t).Equal(true, dbIO.getNullTimeFromString(testString).Valid)
+	}
 }
