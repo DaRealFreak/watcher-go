1c0d29debf655d0cb5c95e594df05a7f7fa1e9d7
Steffen Keuper <steffen.keuper@web.de>
2019-12-18 12:41:29 +0100

    [TASK] make expiration date nullable and optional, extend queries and tests by nulled expiration dates

--- pkg/database/cookies_test.go
+++ pkg/database/cookies_test.go
@@ -19,60 +19,76 @@ func seedCookiesTable(t *testing.T) {
 	assert.New(t).NoError(err)
 
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
-		INSERT INTO cookies (uid, name, value, expiration, module, disabled)
-		VALUES (1, 'future_cookie', 'test123456', '%d', 'test.module', 0);`,
+		INSERT INTO cookies (name, expiration, module)
+		VALUES ('future_cookie', '%d', 'test.module');`,
 		futureTimestamp,
 	))
 	assert.New(t).NoError(err)
 
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
-		INSERT INTO cookies (uid, name, value, expiration, module, disabled)
-		VALUES (2, 'current_cookie', 'test123456', '%d', 'test.module', 0);`,
+		INSERT INTO cookies (name, expiration, module)
+		VALUES ('current_cookie', '%d', 'test.module');`,
 		currentTimestamp,
 	))
 	assert.New(t).NoError(err)
 
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
-		INSERT INTO cookies (uid, name, value, expiration, module, disabled)
-		VALUES (3, 'expired_cookie', 'test123456', '%d', 'test.module', 0);`,
+		INSERT INTO cookies (name, expiration, module)
+		VALUES ('expired_cookie', '%d', 'test.module');`,
 		pastTimestamp,
 	))
 	assert.New(t).NoError(err)
 
+	_, err = dbIO.connection.Exec(`
+		INSERT INTO cookies (name, expiration, module)
+		VALUES ('null_cookie', null, 'test.module');`,
+	)
+	assert.New(t).NoError(err)
+
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
-		INSERT INTO cookies (uid, name, value, expiration, module, disabled)
-		VALUES (4, 'future_cookie_2', 'test123456', '%d', 'test.module.2', 0);`,
+		INSERT INTO cookies (name, expiration, module)
+		VALUES ('future_cookie_2', '%d', 'test.module.2');`,
 		futureTimestamp,
 	))
 	assert.New(t).NoError(err)
 
 	_, err = dbIO.connection.Exec(fmt.Sprintf(`
-		INSERT INTO cookies (uid, name, value, expiration, module, disabled)
-		VALUES (5, 'expired_cookie_2', 'test123456', '%d', 'test.module.2', 0);`,
+		INSERT INTO cookies (name, expiration, module)
+		VALUES ('expired_cookie_2', '%d', 'test.module.2');`,
 		pastTimestamp,
 	))
 	assert.New(t).NoError(err)
+
+	_, err = dbIO.connection.Exec(`
+		INSERT INTO cookies (name, expiration, module)
+		VALUES ('null_cookie', null, 'test.module.2');`,
+	)
+	assert.New(t).NoError(err)
+
 }
 
 func TestDbIO_GetCookie(t *testing.T) {
 	seedCookiesTable(t)
 
 	cookie := dbIO.GetCookie("future_cookie", &models.Module{Key: "test.module"})
-	assert.New(t).NotNil(cookie)
+	assert.New(t).NotEmpty(cookie)
 
 	cookie = dbIO.GetCookie("current_cookie", &models.Module{Key: "test.module"})
 	assert.New(t).Empty(cookie)
 
 	cookie = dbIO.GetCookie("expired_cookie", &models.Module{Key: "test.module"})
 	assert.New(t).Empty(cookie)
+
+	cookie = dbIO.GetCookie("null_cookie", &models.Module{Key: "test.module"})
+	assert.New(t).NotEmpty(cookie)
 }
 
 func TestDbIO_GetAllCookies(t *testing.T) {
 	seedCookiesTable(t)
 
 	cookies := dbIO.GetAllCookies(&models.Module{Key: "test.module"})
-	assert.New(t).Equal(1, len(cookies))
+	assert.New(t).Equal(2, len(cookies))
 
 	cookies = dbIO.GetAllCookies(nil)
-	assert.New(t).Equal(2, len(cookies))
+	assert.New(t).Equal(4, len(cookies))
 }
