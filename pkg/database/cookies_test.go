d420e57fe9ee3b0a25fa3a7097b63a11a6b1d068
DaRealFreak <steffen.keuper@web.de>
2019-12-18 02:33:03 +0100

    [TASK] implement unit tests for cookies getter

---
+++ pkg/database/cookies_test.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"github.com/DaRealFreak/watcher-go/pkg/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDbIO_GetCookies(t *testing.T) {
+	_, err := dbIO.connection.Exec(`
+		INSERT INTO cookies (uid, name, value, expiration, module, disabled) 
+		VALUES (1, 'PHPSESSID', '123456_test123456', '1608256840000', 'test.module', 0);`,
+	)
+	assert.New(t).NoError(err)
+
+	_, err = dbIO.connection.Exec(`
+		INSERT INTO cookies (uid, name, value, expiration, module, disabled) 
+		VALUES (2, 'device_token', 'test123456', '1608256840000', 'test.module', 0);`,
+	)
+	assert.New(t).NoError(err)
+
+	cookies := dbIO.GetCookies(&models.Module{Key: "test.module"})
+	assert.New(t).Equal(len(cookies), 2)
+}
