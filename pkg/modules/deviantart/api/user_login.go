8c636b7abda9875fc6ea1236cd19e78b93e3b8e0
Steffen Keuper <steffen.keuper@web.de>
2019-12-20 16:35:21 +0100

    [TASK] add functionality to check passed HTTP response for CSRF token to use

---
+++ pkg/modules/deviantart/api/user_login.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"encoding/json"
+	"github.com/PuerkitoBio/goquery"
+	"net/http"
+	"regexp"
+	"strconv"
+)
+
+// loginInfo contains all relevant information from the login page
+type loginInfo struct {
+	CSRFToken string `json:"csrfToken"`
+}
+
+// getLoginCSRFToken returns the CSRF token from the login site to use in our POST login request
+func (g ImplicitGrantDeviantart) getLoginCSRFToken(res *http.Response) (*loginInfo, error) {
+	var loginInfo loginInfo
+
+	jsonPattern := regexp.MustCompile(`JSON.parse\((?P<Number>.*csrfToken.*?)\);`)
+
+	document, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	scriptTags := document.Find("script")
+	scriptTags.Each(func(row int, selection *goquery.Selection) {
+		// no need for further checks if we already have our login info
+		if loginInfo.CSRFToken != "" {
+			return
+		}
+
+		scriptContent := selection.Text()
+		if jsonPattern.MatchString(scriptContent) {
+			var s string
+
+			s, err = strconv.Unquote(jsonPattern.FindStringSubmatch(scriptContent)[1])
+			if err != nil {
+				return
+			}
+
+			if err := json.Unmarshal([]byte(s), &loginInfo); err != nil {
+				return
+			}
+		}
+	})
+
+	return &loginInfo, err
+}
