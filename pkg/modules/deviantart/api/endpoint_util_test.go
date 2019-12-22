32c0724de4627a4a7d641eb694597e68a875ea31
DaRealFreak <steffen.keuper@web.de>
2019-12-22 05:36:35 +0100

    [TASK] extend test by API console exploit

--- pkg/modules/deviantart/api/endpoint_util_test.go
+++ pkg/modules/deviantart/api/endpoint_util_test.go
@@ -12,4 +12,14 @@ func TestDeviantartAPI_Placebo(t *testing.T) {
 	placebo, err := daAPI.Placebo()
 	assert.New(t).NoError(err)
 	assert.New(t).Equal("success", placebo.Status)
+
+	// toggle console exploit, we also require the first OAuth2 process to have succeeded
+	// since we require the user information cookie which is set on a successful login
+	daAPI.useConsoleExploit = true
+
+	placeboConsoleExploit, err := daAPI.Placebo()
+	assert.New(t).NoError(err)
+	assert.New(t).Equal("success", placebo.Status)
+
+	assert.New(t).Equal(placebo, placeboConsoleExploit)
 }
