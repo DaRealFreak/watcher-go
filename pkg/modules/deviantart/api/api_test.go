374806eab41aeeee180f9c4d921d9a639965f272
DaRealFreak <steffen.keuper@web.de>
2019-12-22 05:08:54 +0100

    [TASK] compare responses from API console exploit and the normal API

--- pkg/modules/deviantart/api/api_test.go
+++ pkg/modules/deviantart/api/api_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"io/ioutil"
 	"net/url"
 	"os"
 	"testing"
@@ -21,4 +22,20 @@ func TestNewDeviantartAPI(t *testing.T) {
 	res, err := daAPI.request("GET", "/placebo", url.Values{})
 	assert.New(t).NoError(err)
 	assert.New(t).Equal(200, res.StatusCode)
+
+	contentAPI, err := ioutil.ReadAll(res.Body)
+	assert.New(t).NoError(err)
+
+	// toggle console exploit, we also require the first OAuth2 process to have succeeded
+	// since we require the user information cookie which is set on a successful login
+	daAPI.useConsoleExploit = true
+
+	res, err = daAPI.request("GET", "/placebo", url.Values{})
+	assert.New(t).NoError(err)
+	assert.New(t).Equal(200, res.StatusCode)
+
+	contentConsoleExploit, err := ioutil.ReadAll(res.Body)
+	assert.New(t).NoError(err)
+
+	assert.New(t).Equal(contentAPI, contentConsoleExploit)
 }
