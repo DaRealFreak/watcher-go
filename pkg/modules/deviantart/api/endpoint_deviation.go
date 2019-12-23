28b4214cc1d534e24d729754d226fdb645117cc1
Steffen Keuper <steffen.keuper@web.de>
2019-12-23 15:22:15 +0100

    [TASK] implement download and download fallback functions for the deviation endpoint

--- pkg/modules/deviantart/api/endpoint_deviation.go
+++ pkg/modules/deviantart/api/endpoint_deviation.go
@@ -1,12 +1,18 @@
 package api
 
 import (
+	"fmt"
 	"net/url"
 )
 
 // DeviationContent contains all relevant information of the deviation content response of the API
 type DeviationContent struct {
 	HTML string `json:"html"`
+}
+
+// DeviationDownload contains all relevant information of the deviation download response of the API
+type DeviationDownload struct {
+	Src string `json:"src"`
 }
 
 // DeviationContent implements the API endpoint https://www.deviantart.com/api/v1/oauth2/deviation/content
@@ -24,4 +30,37 @@ func (a *DeviantartAPI) DeviationContent(deviationID string) (*DeviationContent,
 	err = a.mapAPIResponse(res, &deviationContent)
 
 	return &deviationContent, err
+}
+
+// DeviationDownload implements the API endpoint
+// https://www.deviantart.com/api/v1/oauth2/deviation/download/{deviationid}
+func (a *DeviantartAPI) DeviationDownload(deviationID string) (*DeviationDownload, error) {
+	res, err := a.request("GET", "/deviation/download/"+url.QueryEscape(deviationID), url.Values{})
+	if err != nil {
+		return nil, err
+	}
+
+	var deviationDownload DeviationDownload
+	err = a.mapAPIResponse(res, &deviationDownload)
+
+	return &deviationDownload, err
+}
+
+// DeviationDownloadFallback is a fallback solution for the API endpoint
+// https://www.deviantart.com/api/v1/oauth2/deviation/download/{deviationid}
+// since the endpoint returns a lot of internal server error responses, while the web interface works properly
+func (a *DeviantartAPI) DeviationDownloadFallback(deviationURL string) (*DeviationDownload, error) {
+	res, err := a.Session.Get(deviationURL)
+	if err != nil {
+		return nil, err
+	}
+
+	dl := a.Session.GetDocument(res).Find(`a[href*="https://www.deviantart.com/download/"]`).First()
+	dlLink, exists := dl.Attr("href")
+
+	if dl.Length() != 1 || !exists {
+		return nil, fmt.Errorf("no download link found in deviation URL: %s", deviationURL)
+	}
+
+	return &DeviationDownload{Src: dlLink}, nil
 }
--- pkg/modules/deviantart/api/endpoint_deviation_test.go
+++ pkg/modules/deviantart/api/endpoint_deviation_test.go
@@ -23,3 +23,34 @@ func TestDeviantartAPI_DeviationContent(t *testing.T) {
 	assert.New(t).NotNil(deviationContentConsoleExploit)
 	assert.New(t).NotEqual("", deviationContentConsoleExploit.HTML)
 }
+
+func TestDeviantartAPI_DeviationDownload(t *testing.T) {
+	daAPI.useConsoleExploit = false
+
+	deviationDownload, err := daAPI.DeviationDownload("E75CDA3B-3160-3CC6-4774-7CD503260472")
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(deviationDownload)
+
+	// toggle console exploit, we also require the first OAuth2 process to have succeeded
+	// since we require the user information cookie which is set on a successful login
+	daAPI.useConsoleExploit = true
+
+	deviationDownloadConsoleExploit, err := daAPI.DeviationDownload("E75CDA3B-3160-3CC6-4774-7CD503260472")
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(deviationDownloadConsoleExploit)
+}
+
+func TestDeviantartAPI_DeviationDownloadFallback(t *testing.T) {
+	// special case for web frontend requests:
+	// the cookie will only get set on the first OAuth2 authentication since it'll get set during the round trip
+	// where the used cookies for the request are already set, so the request would not display a download link
+	// so we call the placebo function for the OAuth2 process, this special case does not occur in the real application
+	_, err := daAPI.Placebo()
+	assert.New(t).NoError(err)
+
+	deviationDownload, err := daAPI.DeviationDownloadFallback(
+		"https://www.deviantart.com/yakovlev-vad/art/Landscape-3-500009916",
+	)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(deviationDownload)
+}
