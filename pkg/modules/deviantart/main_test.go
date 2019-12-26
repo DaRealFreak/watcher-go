4cc1af9a5cd35de3f3dd389c56e231d8e5644dfe
Steffen Keuper <steffen.keuper@web.de>
2019-12-26 20:17:51 +0100

    [TASK] add tests for matching and differentiating between passed URLs with the patterns

---
+++ pkg/modules/deviantart/main_test.go
@@ -0,0 +1,61 @@
+package deviantart
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"regexp"
+	"testing"
+)
+
+const (
+	KeyFeed       = "feed"
+	KeyGallery    = "gallery"
+	KeyAll        = "all"
+	KeyCollection = "collection"
+	KeyTag        = "tag"
+)
+
+func testPattern(t *testing.T, expectedGroup string, pattern *regexp.Regexp) {
+	urls := map[string][]string{
+		KeyFeed: {
+			"https://www.deviantart.com", "https://www.deviantart.com/", "DeviantArt://watchfeed",
+		},
+		KeyGallery: {
+			"https://www.deviantart.com/test/gallery/12345/test",
+			"https://www.deviantart.com/test/gallery/12345/test/",
+			"https://www.deviantart.com/test/gallery/12345", "https://www.deviantart.com/test/gallery/12345/",
+		},
+		KeyAll: {
+			"https://www.deviantart.com/test", "https://www.deviantart.com/test/",
+			"https://www.deviantart.com/test/gallery", "https://www.deviantart.com/test/gallery/",
+			"https://www.deviantart.com/test/gallery/all", "https://www.deviantart.com/test/gallery/all/",
+		},
+		KeyCollection: {
+			"https://www.deviantart.com/testuser/favourites/12345/test-title",
+			"https://www.deviantart.com/testuser/favourites/12345/test-title/",
+			"https://www.deviantart.com/testuser/favourites/12345",
+			"https://www.deviantart.com/testuser/favourites/12345/",
+		},
+		KeyTag: {
+			"https://www.deviantart.com/tag/test", "https://www.deviantart.com/tag/test/",
+		},
+	}
+
+	for grp, grpURLs := range urls {
+		for _, url := range grpURLs {
+			if (grp == expectedGroup) != pattern.MatchString(url) {
+				fmt.Println(pattern.String())
+				fmt.Println(grp, expectedGroup, url)
+			}
+			assert.New(t).Equal(grp == expectedGroup, pattern.MatchString(url))
+		}
+	}
+}
+
+func TestDeviantArt_Pattern(t *testing.T) {
+	patterns := getDeviantArtPattern()
+	testPattern(t, KeyFeed, patterns.feedPattern)
+	testPattern(t, KeyGallery, patterns.galleryPattern)
+	testPattern(t, KeyCollection, patterns.collectionPattern)
+	testPattern(t, KeyTag, patterns.tagPattern)
+}
