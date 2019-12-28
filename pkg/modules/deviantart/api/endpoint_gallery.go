7d793a179433fc114bc1d26095c00ff57145723b
Steffen Keuper <steffen.keuper@web.de>
2019-12-28 12:41:21 +0100

    [TASK] change folder UUID to folder ID, add test for Gallery function

--- pkg/modules/deviantart/api/endpoint_gallery.go
+++ pkg/modules/deviantart/api/endpoint_gallery.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"fmt"
 	"net/url"
 	"strconv"
 )
@@ -10,43 +11,17 @@ type GalleryResponse struct {
 	PaginatedResults
 }
 
-// Gallery implements the API endpoint https://www.deviantart.com/api/v1/oauth2/gallery/{folderUUID}
-func (a *DeviantartAPI) GalleryByFolderID(user string, folderID int, offset uint, limit uint) (*GalleryResponse, error) {
-	folderUUID, err := a.FolderIDToUUID(user, folderID)
-	if err != nil {
-		return nil, err
-	}
-
+// Gallery implements the API endpoint https://www.deviantart.com/api/v1/oauth2/gallery/{folderID}
+func (a *DeviantartAPI) Gallery(user string, folderID uint, offset uint, limit uint) (*GalleryResponse, error) {
 	values := url.Values{
 		"username": {user},
-		"folderid": {folderUUID},
+		"folderid": {strconv.Itoa(int(folderID))},
 		"mode":     {"newest"},
 		"offset":   {strconv.Itoa(int(offset))},
 		"limit":    {strconv.Itoa(int(limit))},
 	}
 
-	res, err := a.request("GET", "/gallery/"+folderUUID, values)
-	if err != nil {
-		return nil, err
-	}
-
-	var galleryAll GalleryResponse
-	err = a.mapAPIResponse(res, &galleryAll)
-
-	return &galleryAll, err
-}
-
-// Gallery implements the API endpoint https://www.deviantart.com/api/v1/oauth2/gallery/{folderUUID}
-func (a *DeviantartAPI) Gallery(user string, folderUUID string, offset uint, limit uint) (*GalleryResponse, error) {
-	values := url.Values{
-		"username": {user},
-		"folderid": {folderUUID},
-		"mode":     {"newest"},
-		"offset":   {strconv.Itoa(int(offset))},
-		"limit":    {strconv.Itoa(int(limit))},
-	}
-
-	res, err := a.request("GET", "/gallery/"+folderUUID, values)
+	res, err := a.request("GET", fmt.Sprintf("/gallery/%d", folderID), values)
 	if err != nil {
 		return nil, err
 	}
--- pkg/modules/deviantart/api/endpoint_gallery_test.go
+++ pkg/modules/deviantart/api/endpoint_gallery_test.go
@@ -6,25 +6,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDeviantartAPI_GalleryAll(t *testing.T) {
+func TestDeviantartAPI_Gallery(t *testing.T) {
 	daAPI.useConsoleExploit = false
 
-	collection, err := daAPI.GalleryAll(
-		"CLG-Artisa", 0, MaxDeviationsPerPage,
+	gallery, err := daAPI.Gallery(
+		"CLG-Artisa", 80472763, 0, MaxDeviationsPerPage,
 	)
 	assert.New(t).NoError(err)
-	assert.New(t).NotNil(collection)
+	assert.New(t).NotNil(gallery)
 
 	// toggle console exploit, we also require the first OAuth2 process to have succeeded
 	// since we require the user information cookie which is set on a successful login
 	daAPI.useConsoleExploit = true
 
-	tagResultsConsoleExploit, err := daAPI.GalleryAll(
+	galleryConsoleExploit, err := daAPI.Gallery(
+		"CLG-Artisa", 80472763, 0, MaxDeviationsPerPage,
+	)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(galleryConsoleExploit)
+
+	assert.New(t).Equal(MaxDeviationsPerPage, len(gallery.Results))
+	assert.New(t).Equal(MaxDeviationsPerPage, len(galleryConsoleExploit.Results))
+}
+
+func TestDeviantartAPI_GalleryAll(t *testing.T) {
+	daAPI.useConsoleExploit = false
+
+	gallery, err := daAPI.GalleryAll(
 		"CLG-Artisa", 0, MaxDeviationsPerPage,
 	)
 	assert.New(t).NoError(err)
-	assert.New(t).NotNil(tagResultsConsoleExploit)
+	assert.New(t).NotNil(gallery)
 
-	assert.New(t).Equal(MaxDeviationsPerPage, len(collection.Results))
-	assert.New(t).Equal(MaxDeviationsPerPage, len(tagResultsConsoleExploit.Results))
+	// toggle console exploit, we also require the first OAuth2 process to have succeeded
+	// since we require the user information cookie which is set on a successful login
+	daAPI.useConsoleExploit = true
+
+	galleryConsoleExploit, err := daAPI.GalleryAll(
+		"CLG-Artisa", 0, MaxDeviationsPerPage,
+	)
+	assert.New(t).NoError(err)
+	assert.New(t).NotNil(galleryConsoleExploit)
+
+	assert.New(t).Equal(MaxDeviationsPerPage, len(gallery.Results))
+	assert.New(t).Equal(MaxDeviationsPerPage, len(galleryConsoleExploit.Results))
 }
