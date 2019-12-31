4f051a8014c6a654b139d10bf6e79f956ada26d1
DaRealFreak <steffen.keuper@web.de>
2019-12-31 01:53:48 +0100

    [TASK] implement general collection functionality

---
+++ pkg/modules/deviantart/collection.go
@@ -0,0 +1,78 @@
+package deviantart
+
+import (
+	"path"
+	"strconv"
+
+	"github.com/DaRealFreak/watcher-go/pkg/models"
+	"github.com/DaRealFreak/watcher-go/pkg/modules/deviantart/api"
+)
+
+func (m *deviantArt) parseCollection(item *models.TrackedItem) error {
+	var downloadQueue []downloadQueueItem
+
+	currentItemID, _ := strconv.ParseInt(item.CurrentItem, 10, 64)
+	foundCurrentItem := false
+	username := m.daPattern.collectionPattern.FindStringSubmatch(item.URI)[1]
+	collectionID := m.daPattern.collectionPattern.FindStringSubmatch(item.URI)[2]
+	collectionIntID, _ := strconv.ParseInt(collectionID, 10, 64)
+
+	collectionName, err := m.daAPI.CollectionNameFromURL(item.URI)
+	if err != nil {
+		return err
+	} else if collectionName == "All" {
+		return m.parseAllCollections(username)
+	}
+
+	collectionUUID, err := m.daAPI.CollectionFolderIDToUUID(username, int(collectionIntID))
+	if err != nil {
+		return err
+	}
+
+	response, err := m.daAPI.Collection(username, collectionUUID, 0, api.MaxDeviationsPerPage)
+	if err != nil {
+		return err
+	}
+
+	for !foundCurrentItem {
+		for _, deviation := range response.Results {
+			publishedTime, err := strconv.ParseInt(deviation.PublishedTime, 10, 64)
+			if err != nil {
+				return err
+			}
+
+			if item.CurrentItem == "" || publishedTime > currentItemID {
+				downloadQueue = append(downloadQueue, downloadQueueItem{
+					itemID:      deviation.PublishedTime,
+					deviation:   deviation,
+					downloadTag: path.Join(m.SanitizePath(username, false), collectionName),
+				})
+			} else {
+				foundCurrentItem = true
+				break
+			}
+		}
+
+		if response.NextOffset == nil {
+			break
+		}
+
+		response, err = m.daAPI.Collection(username, collectionUUID, *response.NextOffset, api.MaxDeviationsPerPage)
+		if err != nil {
+			return err
+		}
+	}
+
+	for i, j := 0, len(downloadQueue)-1; i < j; i, j = i+1, j-1 {
+		downloadQueue[i], downloadQueue[j] = downloadQueue[j], downloadQueue[i]
+	}
+
+	return m.processDownloadQueue(downloadQueue, item)
+}
+
+// parseAllCollections parses all favourites of the passed user
+// this is a special case since the API won't return the "All" folder and we can't retrieve the folder ID
+// with the Eclipse theme which is enforced for new users....
+func (m *deviantArt) parseAllCollections(username string) error {
+	return nil
+}
