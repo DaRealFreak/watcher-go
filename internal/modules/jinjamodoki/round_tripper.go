ab4495fe4414b240d55ef584d74c2e00163be1dd
DaRealFreak <steffen.keuper@web.de>
2020-01-11 04:09:00 +0100

    [TASK] add random user agent, origin and accept headers to make requests generally more believable

--- internal/modules/jinjamodoki/round_tripper.go
+++ internal/modules/jinjamodoki/round_tripper.go
@@ -1,6 +1,10 @@
 package jinjamodoki
 
-import "net/http"
+import (
+	"net/http"
+
+	browser "github.com/EDDYCJY/fake-useragent"
+)
 
 // round tripper for custom user agent header
 type refererRoundTripper struct {
@@ -16,7 +20,11 @@ func (m *jinjaModoki) SetReferer(inner http.RoundTripper) http.RoundTripper {
 
 // RoundTrip adds the custom user agent to request headers
 func (m *refererRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	r.Header.Set("Accept-Language", "en-US,en;q=0.5")
+	r.Header.Set("Origin", "https://gs-uploader.jinja-modoki.com")
 	r.Header.Set("Referer", "https://gs-uploader.jinja-modoki.com/")
+	r.Header.Set("User-Agent", browser.Firefox())
 
 	if m.inner == nil {
 		return http.DefaultTransport.RoundTrip(r)
