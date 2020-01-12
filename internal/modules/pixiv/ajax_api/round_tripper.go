a9b981a0053b3624628c2c6c19bd2d7b23112bea
DaRealFreak <steffen.keuper@web.de>
2020-01-12 21:12:58 +0100

    [BUGFIX] remove the accept encoding header to let the go http library handle the encoding

--- internal/modules/pixiv/ajax_api/round_tripper.go
+++ internal/modules/pixiv/ajax_api/round_tripper.go
@@ -24,7 +24,6 @@ func (a *AjaxAPI) setPixivWebHeaders(inner http.RoundTripper, sessionCookie *mod
 // RoundTrip adds the required request headers to pass CloudFlare checks
 func (rt *pixivRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	r.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-	r.Header.Set("Accept-Encoding", "gzip, deflate, br")
 	r.Header.Set("Accept-Language", "en-US,en;q=0.5")
 	r.Header.Set("User-Agent", browser.Firefox())
 	r.Header.Set("Referer", "https://www.pixiv.net/")
