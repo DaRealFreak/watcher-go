d59a9d27d00a1d89f24748a79fba011133766d26
DaRealFreak <steffen.keuper@web.de>
2021-11-16 15:55:57 +0100

    [TASK] only apply headers if the requested URL host is also deviantart

--- internal/modules/deviantart/api/round_tripper.go
+++ internal/modules/deviantart/api/round_tripper.go
@@ -5,22 +5,25 @@ import (
 )
 
 type deviantArtConsoleAPIRoundTripper struct {
-	inner   http.RoundTripper
+	inner http.RoundTripper
 }
 
 // setDeviantArtHeaders returns the round tripper for the DeviantArt console API headers
 func (a *DeviantartAPI) setDeviantArtHeaders(inner http.RoundTripper) http.RoundTripper {
 	return &deviantArtConsoleAPIRoundTripper{
-		inner:   inner,
+		inner: inner,
 	}
 }
 
 // RoundTrip adds the required request headers to pass server side checks of DeviantArt
 func (rt *deviantArtConsoleAPIRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	r.Header.Set("Accept-Language", "en-US,en;q=0.5")
-	r.Header.Set("Host", "https://www.deviantart.com")
-	r.Header.Set("Referer", "https://www.deviantart.com")
-	r.Header.Set("Origin", "https://www.deviantart.com")
+
+	if r.URL.Host == "www.deviantart.com" {
+		r.Header.Set("Host", "https://www.deviantart.com")
+		r.Header.Set("Referer", "https://www.deviantart.com")
+		r.Header.Set("Origin", "https://www.deviantart.com")
+	}
 
 	if rt.inner == nil {
 		return http.DefaultTransport.RoundTrip(r)
