ff72467fd9dcaa964b24d11d2c1d4b15de38efbc
DaRealFreak <steffen.keuper@web.de>
2022-08-10 03:16:38 +0200

    [BUGFIX] fix truncation of string lengths for prefixed paths

--- internal/models/module_truncate_windows.go
+++ internal/models/module_truncate_windows.go
@@ -10,8 +10,16 @@ import (
 
 // TruncateMaxLength checks for length of the passed path part to ensure the max path length
 func (t Module) TruncateMaxLength(s string) string {
-	if syscall.MAX_PATH > len(s) {
+	// -5 for prefixes and null byte ending
+	if syscall.MAX_PATH-5 > len(s) {
 		return s
 	}
-	return s[:strings.LastIndex(s[:syscall.MAX_PATH], " ")]
+
+	if strings.Contains(s, " ") {
+		// prefer splitting the string on the last space
+		return s[:strings.LastIndex(s[:syscall.MAX_PATH-5], " ")]
+	} else {
+		// else hard cut the string
+		return s[:syscall.MAX_PATH-5]
+	}
 }
