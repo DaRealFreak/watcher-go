0296f09ee2a9dba19e95d3a178435c1fe4001376
DaRealFreak <steffen.keuper@web.de>
2022-11-17 04:00:42 +0100

    [BUGFIX] import os for unix

--- pkg/fp/filepath_unix.go
+++ pkg/fp/filepath_unix.go
@@ -2,6 +2,10 @@
 
 package fp
 
+import (
+	"os"
+)
+
 // MoveFile uses syscall to move file (even across drives, which isn't allowed normally for windows)
 func MoveFile(src string, dst string) error {
 	return os.Rename(src, dst)
